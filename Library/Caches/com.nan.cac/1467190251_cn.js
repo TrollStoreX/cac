19b94e093a3cfd95a3c68894873a95d0c7f1c08c
GitHub Action <action@github.com>
2024-06-24 02:27:14 +0000

    save caches

---
+++ Library/Caches/com.nan.cac/1467190251_cn.js
@@ -0,0 +1,9 @@
+{
+  "status": 0,
+  "bundle": "com.miHoYo.Yuanshen",
+  "id": 1467190251,
+  "name": "原神",
+  "version": "4.7.0",
+  "price": "免费",
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/48/3a/c3/483ac35f-fa03-dded-85b6-65936cac968e/AppIcon-1x_U007epad-0-0-85-220-0.png/100x100bb.jpg"
+}
---
+++ Library/Caches/com.nan.cac/686449807_us.js
@@ -0,0 +1,9 @@
+{
+  "status": 0,
+  "bundle": "ph.telegra.Telegraph",
+  "id": 686449807,
+  "name": "Telegram Messenger",
+  "version": 10.14,
+  "price": "Free",
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/02/f7/56/02f756c7-d30b-3752-09c1-b8a1e23b6823/AppIconLLC-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
+}
