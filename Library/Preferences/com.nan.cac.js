29ba12a37d58254223ca1990457d9c9db6498a6a
zhaonan <35675770+invalidunit@users.noreply.github.com>
2024-06-24 10:09:55 +0800

    Create com.nan.cac.js

---
+++ Library/Preferences/com.nan.cac.js
@@ -0,0 +1,74 @@
+{
+  "app": [
+    {
+      "enable": 1,
+      "name": "Bark",
+      "url": "https://apps.apple.com/cn/app/id1403753865"
+    },
+    {
+      "enable": 1,
+      "name": "QQ",
+      "url": "https://apps.apple.com/cn/app/id444934666"
+    },
+    {
+      "enable": 1,
+      "name": "微信",
+      "url": "https://apps.apple.com/cn/app/id414478124"
+    },
+    {
+      "enable": 1,
+      "name": "Telegram",
+      "url": "https://apps.apple.com/us/app/id686449807"
+    },
+    {
+      "enable": 1,
+      "name": "抖音",
+      "url": "https://apps.apple.com/cn/app/id1142110895"
+    },
+    {
+      "enable": 1,
+      "name": "原神",
+      "url": "https://apps.apple.com/cn/app/id1467190251"
+    },
+    {
+      "enable": 1,
+      "name": "崩坏：星穹铁道",
+      "url": "https://apps.apple.com/cn/app/id1523037824"
+    },
+    {
+      "enable": 1,
+      "name": "第五人格",
+      "url": "https://apps.apple.com/cn/app/id1330863325"
+    },
+    {
+      "enable": 1,
+      "name": "Alook",
+      "url": "https://apps.apple.com/cn/app/id1261944766",
+      "bark": {
+        "body": {
+          "cache": "当前最新版本：app_version\\n当前购买价格：app_price\\n已缓存到本地等待下一次对比",
+          "del": "app_bundle\\n可能已下架，最后出现版本为：\\napp_version\\n从 cac 配置文件中移除 app_id 以避免不必要的消耗",
+          "up": "发现变更：\\n版本：app_version\\n价格：app_price"
+        }
+      }
+    }
+  ],
+  "enable": 1,
+  "bark": {
+    "is_archive": 1,
+    "Hello_World": 0,
+    "level": "timeSensitive",
+    "title": {
+      "cache": "来自 cac - app_name",
+      "del": "来自 cac - app_name",
+      "up": "来自 cac - app_name"
+    },
+    "group": "com.nan.cac",
+    "body": {
+      "cache": "当前最新版本：app_version\\n已缓存到本地等待下一次对比",
+      "del": "app_bundle\\n可能已下架，最后出现版本为：\\napp_version\\n从 cac 配置文件中移除 app_id 以避免不必要的消耗",
+      "up": "发现变更：\\n版本：app_version",
+    },
+    "sound": "healthnotification"
+  }
+}
