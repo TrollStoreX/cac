4090ff89bce6ba6a29bad159912c8c980f8b67a5
GitHub Action <action@github.com>
2024-07-17 22:47:28 +0000

    save caches

--- Library/Caches/com.nan.cac/1467190251_cn.js
+++ Library/Caches/com.nan.cac/1467190251_cn.js
@@ -5,5 +5,5 @@
   "name": "原神",
   "version": "4.8.0",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/48/3a/c3/483ac35f-fa03-dded-85b6-65936cac968e/AppIcon-1x_U007epad-0-0-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/10/20/65/102065c0-6861-5ee9-b511-6e075ee0a9f5/AppIcon-1x_U007epad-0-0-85-220-0.png/100x100bb.jpg"
 }
