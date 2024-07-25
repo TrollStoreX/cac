75de47a33657c4b76aaa03867b2d3163dff912ea
gujiaming2022 <59131675+gujiaming2022@users.noreply.github.com>
2024-07-25 22:42:19 +0800

    Delete Library/Caches/com.nan.cac directory

--- Library/Caches/com.nan.cac/686449807_us.js
+++
@@ -1,9 +0,0 @@
-{
-  "status": 0,
-  "bundle": "ph.telegra.Telegraph",
-  "id": 686449807,
-  "name": "Telegram Messenger",
-  "version": "10.14.2",
-  "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/bd/e8/15/bde81571-2158-dba8-b061-dfd313747493/AppIconLLC-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
-}
