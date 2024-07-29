df57c39f91b340b01329af0932aef3b5e43bc257
gujiaming2022 <59131675+gujiaming2022@users.noreply.github.com>
2024-07-29 14:47:21 +0800

    Delete Library/Caches/com.nan.cac/324684580_us.js

--- Library/Caches/com.nan.cac/324684580_us.js
+++
@@ -1,9 +0,0 @@
-{
-  "status": 0,
-  "bundle": "com.spotify.client",
-  "id": 324684580,
-  "name": "Spotify - Music and Podcasts",
-  "version": "8.9.58",
-  "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/02/da/83/02da839d-4c98-3e32-5585-9850b867e810/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
-}
