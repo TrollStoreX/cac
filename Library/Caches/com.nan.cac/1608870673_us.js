d048d0da907d481ff4b7f5fa7bc8d9841295f0f8
GitHub Action <action@github.com>
2024-08-05 07:24:54 +0000

    save caches

--- Library/Caches/com.nan.cac/1608870673_us.js
+++ Library/Caches/com.nan.cac/1608870673_us.js
@@ -3,7 +3,7 @@
   "bundle": "app.nicegram",
   "id": 1608870673,
   "name": "Nicegram: AI Chat for Telegram",
-  "version": "1.6.8",
+  "version": "1.6.9",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/30/f4/50/30f45079-c7de-858d-c031-4bc70d6955ec/AppIconLLC-0-0-1x_U007epad-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/43/d8/46/43d846d8-15f6-7e56-6b71-010028a6e4ce/AppIconLLC-0-0-1x_U007epad-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.xingin.discover",
   "id": 741292507,
   "name": "小红书",
-  "version": 8.47,
+  "version": 8.48,
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/52/62/d9/5262d981-a3cf-21e6-479e-37570ed7bba1/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
