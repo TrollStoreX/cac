a3b346cff27f7ddc766d36f6307db3016d52eafe
GitHub Action <action@github.com>
2024-08-20 04:50:11 +0000

    save caches

--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -5,5 +5,5 @@
   "name": "YouTube: Watch, Listen, Stream",
   "version": "19.33.2",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/81/d9/af/81d9af91-e046-8839-67d3-99271a5977dc/logo_youtube_color-0-1x_U007emarketing-0-0-0-6-0-0-0-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/69/a8/bc/69a8bca1-dcc3-f61d-869e-c49ff2d4768d/logo_youtube_color-0-1x_U007emarketing-0-0-0-6-0-0-0-85-220-0.png/100x100bb.jpg"
 }
