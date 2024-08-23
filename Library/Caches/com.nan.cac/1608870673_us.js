b65c2b744e140abb49d59bc8d392d4a7308ce1f5
GitHub Action <action@github.com>
2024-08-23 09:27:26 +0000

    save caches

--- Library/Caches/com.nan.cac/1608870673_us.js
+++ Library/Caches/com.nan.cac/1608870673_us.js
@@ -3,7 +3,7 @@
   "bundle": "app.nicegram",
   "id": 1608870673,
   "name": "Nicegram: AI Chat for Telegram",
-  "version": "1.7.5",
+  "version": "1.7.6",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/36/53/ad/3653addc-df75-589d-b270-b04ff8c7645b/AppIconLLC-0-0-1x_U007epad-0-0-85-220.png/100x100bb.jpg"
 }
