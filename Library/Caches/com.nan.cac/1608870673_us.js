61587930b93038962f3faaf25a9bc7e37863dbc2
GitHub Action <action@github.com>
2024-08-24 06:33:27 +0000

    save caches

--- Library/Caches/com.nan.cac/1608870673_us.js
+++ Library/Caches/com.nan.cac/1608870673_us.js
@@ -5,5 +5,5 @@
   "name": "Nicegram: AI Chat for Telegram",
   "version": "1.7.6",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/f3/73/ba/f373ba6f-8eaf-eb44-2287-a1a625bc133a/AppIconLLC-0-0-1x_U007epad-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/36/53/ad/3653addc-df75-589d-b270-b04ff8c7645b/AppIconLLC-0-0-1x_U007epad-0-0-85-220.png/100x100bb.jpg"
 }
