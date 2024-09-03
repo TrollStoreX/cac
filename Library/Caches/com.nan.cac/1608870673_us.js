45e8a1ef82d5acb41b304426d4fe03fb1d17c54b
GitHub Action <action@github.com>
2024-09-03 12:49:37 +0000

    save caches

--- Library/Caches/com.nan.cac/1608870673_us.js
+++ Library/Caches/com.nan.cac/1608870673_us.js
@@ -3,7 +3,7 @@
   "bundle": "app.nicegram",
   "id": 1608870673,
   "name": "Nicegram: Chat for Telegram X",
-  "version": "1.7.7",
+  "version": "1.7.8",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/33/0b/06/330b0629-0845-ee15-9995-f6f72176b363/AppIconLLC-0-0-1x_U007epad-0-0-85-220.png/100x100bb.jpg"
 }
