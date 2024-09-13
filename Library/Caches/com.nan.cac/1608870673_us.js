19878f57d3998d662ab8b86001901d8d2ec0bcd4
GitHub Action <action@github.com>
2024-09-13 07:47:57 +0000

    save caches

--- Library/Caches/com.nan.cac/1608870673_us.js
+++ Library/Caches/com.nan.cac/1608870673_us.js
@@ -3,7 +3,7 @@
   "bundle": "app.nicegram",
   "id": 1608870673,
   "name": "Nicegram: AI for My Telegram X",
-  "version": "1.8.0",
+  "version": "1.8.1",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/05/70/66/057066a9-731c-0e8e-9fd8-b67c05466ed9/AppIconLLC-0-0-1x_U007epad-0-0-85-220.png/100x100bb.jpg"
 }
