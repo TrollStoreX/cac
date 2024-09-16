b279bd751ba3b9574434daaa41fe89e60970dce2
GitHub Action <action@github.com>
2024-09-16 12:52:37 +0000

    save caches

--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.google.ios.youtube",
   "id": 544007664,
   "name": "YouTube: Watch, Listen, Stream",
-  "version": "19.36.1",
+  "version": "19.37.2",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/8b/1c/75/8b1c7513-9bdd-9b4a-15ae-72879d721bc9/logo_youtube_color-0-1x_U007emarketing-0-0-0-6-0-0-0-85-220-0.png/100x100bb.jpg"
 }
