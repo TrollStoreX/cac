98e346b6dcf4257497ccdcc144b40eea8bed1c90
GitHub Action <action@github.com>
2024-09-26 12:54:44 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -5,5 +5,5 @@
   "name": "小红书",
   "version": "8.55.2",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/f3/ad/0f/f3ad0fda-81fe-2d02-a25e-0217190c0271/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/c9/43/cb/c943cbec-29f2-548d-daa2-215ceaaad935/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
