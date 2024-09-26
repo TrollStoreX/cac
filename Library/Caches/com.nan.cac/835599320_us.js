dc44aa0f7f9ae7214e692467b194ec24425be80a
GitHub Action <action@github.com>
2024-09-26 15:31:40 +0000

    save caches

--- Library/Caches/com.nan.cac/835599320_us.js
+++ Library/Caches/com.nan.cac/835599320_us.js
@@ -5,5 +5,5 @@
   "name": "TikTok",
   "version": "36.6.0",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/fa/a1/22/faa12294-f778-79ee-8349-cea7f1057777/AppIcon_TikTok-0-0-1x_U007epad-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/6c/26/7d/6c267dcc-be7e-bfdd-57ca-83ac504ab018/AppIcon_TikTok-0-0-1x_U007epad-0-0-0-85-220.png/100x100bb.jpg"
 }
