7acd92820d896523bfbafd98a5ad45525328dea6
GitHub Action <action@github.com>
2024-10-01 18:38:41 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -5,5 +5,5 @@
   "name": "X",
   "version": 10.61,
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/6b/e2/16/6be2163e-e41e-d221-1f96-f3496e94cd3e/ProductionAppIcon-0-0-1x_U007emarketing-0-7-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/1b/fa/38/1bfa3851-d8d6-f5ad-2105-92e59aefc8e0/ProductionAppIcon-0-0-1x_U007emarketing-0-7-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.burbn.instagram",
   "id": 389801252,
   "name": "Instagram",
-  "version": "350.0.0",
+  "version": "351.0.0",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/45/63/1c/45631cea-499d-7f62-74ce-b282dfdd0a10/Prod-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
 }
