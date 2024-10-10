96d5a09bdf2d914731dc540fd16675e57c5e2784
GitHub Action <action@github.com>
2024-10-10 18:53:48 +0000

    save caches

--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -4,6 +4,6 @@
   "id": 544007664,
   "name": "YouTube: Watch, Listen, Stream",
   "version": "19.40.4",
-  "price": "Free",
+  "price": null,
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/14/33/86/143386d7-546d-d3b4-56d7-fa9bbf6401f4/logo_youtube_color-0-1x_U007emarketing-0-0-0-6-0-0-0-85-220-0.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -4,6 +4,6 @@
   "id": 741292507,
   "name": "小红书",
   "version": 8.56,
-  "price": "Free",
+  "price": null,
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/46/11/89/4611892d-e538-e05b-06e8-e1b648f5e7a3/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
