2f5faa5b9db5732631b45e9f026968dba6772c0a
GitHub Action <action@github.com>
2024-10-11 16:36:34 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -5,5 +5,5 @@
   "name": "酷安-发现科技新生活",
   "version": "5.7.1",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/f4/1a/49/f41a4994-4355-ff5a-f690-ced24350e7cb/AppIcon-0-1x_U007emarketing-0-11-0-0-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/ca/af/72/caaf722c-866c-0e96-71d8-c7f2cff27423/AppIcon-0-1x_U007emarketing-0-10-0-0-85-220-0.png/100x100bb.jpg"
 }
