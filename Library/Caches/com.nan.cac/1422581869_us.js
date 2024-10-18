287026c42ca7548c13d2337e64835a11159ec997
GitHub Action <action@github.com>
2024-10-18 01:57:23 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.coolapk.app",
   "id": 1422581869,
   "name": "酷安-发现科技新生活",
-  "version": "5.7.3",
+  "version": "5.7.4",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c4/fc/8e/c4fc8e13-5b76-61cd-4a19-4e4a81f3d304/AppIcon-0-1x_U007emarketing-0-11-0-0-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/de/43/4f/de434fa4-a37c-8537-9ed5-89f458ac2021/AppIcon-0-1x_U007emarketing-0-11-0-0-85-220-0.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.burbn.instagram",
   "id": 389801252,
   "name": "Instagram",
-  "version": 353,
+  "version": "353.1.0",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/78/f0/73/78f0732e-c392-687a-47e3-937ebbd8eaee/Prod-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
 }
