cb31a685356c7c1829afbb5ec3bc00cd6f8b279a
GitHub Action <action@github.com>
2024-10-22 07:49:23 +0000

    save caches

--- Library/Caches/com.nan.cac/324684580_us.js
+++ Library/Caches/com.nan.cac/324684580_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.spotify.client",
   "id": 324684580,
   "name": "Spotify - Music and Podcasts",
-  "version": "8.9.82",
+  "version": "8.9.84",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/9c/c0/54/9cc05463-2e4f-00a1-d693-7a0bc78ec66c/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -5,5 +5,5 @@
   "name": "Instagram",
   "version": "354.0.0",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/9f/00/8e/9f008ea7-a6f4-8433-2496-1f9cef615b7a/Prod-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/9a/0f/6c/9a0f6c1a-8c12-7f10-02f0-11eb82356ce0/Prod-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
 }
