9a015ee3eae38666a4f0beb21fdd5ec1710c8d8d
GitHub Action <action@github.com>
2024-10-23 07:30:33 +0000

    save caches

---
+++ Library/Caches/com.nan.cac/1373567447_us.js
@@ -0,0 +1,9 @@
+{
+  "status": 0,
+  "bundle": "com.ruikq.decar",
+  "id": 1373567447,
+  "name": "Loon",
+  "version": "3.2.3",
+  "price": "$5.99",
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/1b/1d/b8/1b1db8b1-3587-e80f-2b5c-20acbcb6d3f2/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+}
---
+++ Library/Caches/com.nan.cac/1534690075_us.js
@@ -0,0 +1,9 @@
+{
+  "status": 0,
+  "bundle": "tech.baye.placeholder",
+  "id": 1534690075,
+  "name": "DAMA - Auto Redact Privacy",
+  "version": 1.24,
+  "price": "Free",
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/f9/3d/25/f93d25cf-be86-3e11-a512-1bfabd6073a5/AppIcon-0-1x_U007epad-0-1-0-85-220-0.png/100x100bb.jpg"
+}
