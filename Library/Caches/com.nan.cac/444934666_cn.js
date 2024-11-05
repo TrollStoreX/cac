b6ce62d50c7dcba3238a2e8b0eee5c08e77c0cc2
GitHub Action <action@github.com>
2024-11-05 09:49:06 +0000

    save caches

--- Library/Caches/com.nan.cac/444934666_cn.js
+++ Library/Caches/com.nan.cac/444934666_cn.js
@@ -4,6 +4,6 @@
   "id": 444934666,
   "name": "QQ",
   "version": "9.1.12",
-  "price": null,
+  "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/f2/4c/19/f24c1917-7b31-49e0-e5df-57ea4f9545ea/AppIcon-1-0-1x_U007epad-0-1-0-sRGB-85-220-0.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/6448311069_us.js
+++ Library/Caches/com.nan.cac/6448311069_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.openai.chat",
   "id": 6448311069,
   "name": "ChatGPT",
-  "version": "1.2024.297",
-  "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d5/75/8d/d5758d23-ab1a-cc98-a555-9fd063a45abd/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "version": "1.2024.303",
+  "price": "Free",
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c5/b3/0b/c5b30b22-8679-5c4d-d03e-d5a02bb20c28/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
