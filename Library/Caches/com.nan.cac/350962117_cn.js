e6234bfc18c8d8a70618e039fc465965a2b29972
GitHub Action <action@github.com>
2024-11-06 02:57:09 +0000

    save caches

--- Library/Caches/com.nan.cac/350962117_cn.js
+++ Library/Caches/com.nan.cac/350962117_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.sina.weibo",
   "id": 350962117,
   "name": "微博",
-  "version": "14.10.2",
+  "version": "14.10.3",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/15/29/30/1529306e-9f4a-fca8-1d27-f3f314942efe/WeiboAppIcon-0-0-1x_U007emarketing-0-4-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/6448311069_us.js
+++ Library/Caches/com.nan.cac/6448311069_us.js
@@ -5,5 +5,5 @@
   "name": "ChatGPT",
   "version": "1.2024.304",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d5/75/8d/d5758d23-ab1a-cc98-a555-9fd063a45abd/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/48/87/02/4887022e-9ac1-3f2d-1d71-3829ea5afb05/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
