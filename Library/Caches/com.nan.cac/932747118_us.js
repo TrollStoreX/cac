5ba3cdede2684ab00069b82a8e4d2494981db0cb
GitHub Action <action@github.com>
2024-11-11 23:27:51 +0000

    save caches

--- Library/Caches/com.nan.cac/932747118_us.js
+++ Library/Caches/com.nan.cac/932747118_us.js
@@ -5,5 +5,5 @@
   "name": "Shadowrocket",
   "version": "2.2.57",
   "price": "$2.99",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/2d/ed/6e/2ded6e8b-c9aa-823c-be15-0cdd40777b65/AppIcon-0-0-1x_U007emarketing-0-11-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/16/79/a3/1679a3b3-b4bc-7bc0-660f-09b22ced6c87/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
