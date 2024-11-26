2d7cdc1944d735f009969bed29ea2e68c647c4e7
GitHub Action <action@github.com>
2024-11-26 02:04:27 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.atebits.Tweetie2",
   "id": 333903271,
   "name": "X",
-  "version": "10.68.1",
+  "version": 10.69,
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/32/58/98/325898bb-33d1-70ba-078b-be94985f989e/ProductionAppIcon-0-0-1x_U007emarketing-0-7-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/350962117_cn.js
+++ Library/Caches/com.nan.cac/350962117_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.sina.weibo",
   "id": 350962117,
   "name": "微博",
-  "version": "14.11.1",
+  "version": "14.11.2",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/61/db/c6/61dbc630-8e81-6183-abda-28d8efff95d7/WeiboAppIcon-0-0-1x_U007epad-0-6-0-0-85-220.png/100x100bb.jpg"
 }
