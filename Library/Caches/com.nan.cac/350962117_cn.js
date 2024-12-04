b9ac86ae4b7088c25ba5e7eb5e6ebbe654c810c1
GitHub Action <action@github.com>
2024-12-04 02:06:58 +0000

    save caches

--- Library/Caches/com.nan.cac/350962117_cn.js
+++ Library/Caches/com.nan.cac/350962117_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.sina.weibo",
   "id": 350962117,
   "name": "微博",
-  "version": "14.11.2",
+  "version": "14.11.3",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/6f/5f/3f/6f5f3f76-f2ec-1da0-10d7-779dbb2286b4/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
