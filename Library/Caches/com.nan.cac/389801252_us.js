140c8bc8f27b422552ba701a2aae073f83804a1c
GitHub Action <action@github.com>
2024-12-10 15:37:44 +0000

    save caches

--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -5,5 +5,5 @@
   "name": "Instagram",
   "version": "360.0.0",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/97/9e/c4/979ec480-795b-371e-ff57-79d532283364/Prod-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/2b/94/bd/2b94bdc9-c2b9-811e-f1de-decab8665868/Prod-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/932747118_us.js
+++ Library/Caches/com.nan.cac/932747118_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.liguangming.Shadowrocket",
   "id": 932747118,
   "name": "Shadowrocket",
-  "version": "2.2.59",
+  "version": "2.2.60",
   "price": "$2.99",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/24/23/17/2423174c-c01b-dc68-1a75-34078e41e340/AppIcon-0-0-1x_U007epad-0-1-85-220.png/100x100bb.jpg"
 }
