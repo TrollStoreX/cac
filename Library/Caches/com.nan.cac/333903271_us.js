bf17d4741a8c3d7684b8140e7a564847840f704c
GitHub Action <action@github.com>
2024-12-13 05:53:30 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -5,5 +5,5 @@
   "name": "X",
   "version": "10.71.1",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/fa/e9/86/fae9867c-b191-8adc-f4c8-07a26e1951f0/ProductionAppIcon-0-0-1x_U007emarketing-0-7-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/3f/81/85/3f818590-328f-2753-bee7-4bd711333f8d/ProductionAppIcon-0-0-1x_U007emarketing-0-7-0-0-0-85-220.png/100x100bb.jpg"
 }
