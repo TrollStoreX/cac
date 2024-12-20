d353a04b918c636df2d1f3be5c8b2f7152d108cb
GitHub Action <action@github.com>
2024-12-20 11:27:37 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.xingin.discover",
   "id": 741292507,
   "name": "小红书",
-  "version": "8.66.1",
+  "version": 8.67,
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/66/ab/8b/66ab8b09-4d34-2979-c4ee-ab16088f2169/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/932747118_us.js
+++ Library/Caches/com.nan.cac/932747118_us.js
@@ -5,5 +5,5 @@
   "name": "Shadowrocket",
   "version": "2.2.61",
   "price": "$2.99",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/9b/60/bf/9b60bf7d-5a64-1b53-2a60-d8bfd5b7ecd2/AppIcon-0-0-1x_U007epad-0-1-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/06/78/5e/06785efb-aec6-84ca-cbbc-84c1f4a4f369/AppIcon-0-0-1x_U007epad-0-1-85-220.png/100x100bb.jpg"
 }
