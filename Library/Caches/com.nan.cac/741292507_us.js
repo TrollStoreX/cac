0de0aa35f3dbbf8b6c571b2ba37ca39cbeb39af5
GitHub Action <action@github.com>
2025-01-03 03:38:33 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.xingin.discover",
   "id": 741292507,
   "name": "小红书",
-  "version": 8.67,
+  "version": 8.68,
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/9b/30/9d/9b309d9c-a1d0-ed7d-b6d5-1c055a8e4413/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
