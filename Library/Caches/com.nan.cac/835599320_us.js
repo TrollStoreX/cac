941a943009bbfe3a44288e191793b65422948ed9
GitHub Action <action@github.com>
2025-01-10 16:39:06 +0000

    save caches

--- Library/Caches/com.nan.cac/835599320_us.js
+++ Library/Caches/com.nan.cac/835599320_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.zhiliaoapp.musically",
   "id": 835599320,
   "name": "TikTok",
-  "version": "37.8.0",
+  "version": "38.1.0",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/28/55/b8/2855b8ba-c196-6d59-9d7f-ddc9361282f3/AppIcon_TikTok-0-0-1x_U007epad-0-0-0-85-220.png/100x100bb.jpg"
 }
