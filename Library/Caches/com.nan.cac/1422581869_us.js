95c8095c95a62fb7859bae1f0ec69a5ed4f75939
GitHub Action <action@github.com>
2025-01-12 23:31:03 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.coolapk.app",
   "id": 1422581869,
   "name": "酷安-发现科技新生活",
-  "version": "5.8.4",
+  "version": 15,
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/db/f4/96/dbf4964d-7223-06b2-a778-7dd2c67dfa01/AppIcon-0-0-1x_U007emarketing-0-11-0-0-85-220.png/100x100bb.jpg"
 }
