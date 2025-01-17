218b6b22d6da62ff07569d7cb7848f0fb3ea025f
GitHub Action <action@github.com>
2025-01-17 02:57:32 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -5,5 +5,5 @@
   "name": "X",
   "version": "10.76.1",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/23/40/63/234063fb-ac56-d2ea-301e-40d3bd5f9c7f/ProductionAppIcon-0-0-1x_U007emarketing-0-7-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/44/84/3f/44843fe3-9c7c-2fbd-6629-aaddfbb76dac/ProductionAppIcon-0-0-1x_U007emarketing-0-7-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.xingin.discover",
   "id": 741292507,
   "name": "小红书",
-  "version": "8.69.1",
+  "version": "8.69.2",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/cc/a3/45/cca345e2-78b4-8901-bbf2-12003f3355dd/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
