001e1aba139b49e6c27349c88a30df49323257b5
GitHub Action <action@github.com>
2025-01-21 08:54:47 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -2,8 +2,8 @@
   "status": 0,
   "bundle": "com.xingin.discover",
   "id": 741292507,
-  "name": "rednote - share, connect, love",
+  "name": "小红书",
   "version": "8.69.3",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/b2/bc/8a/b2bc8a51-42a0-c73c-470f-4d90427cdbe9/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/37/ad/d0/37add097-e740-b971-5e64-215408506319/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
