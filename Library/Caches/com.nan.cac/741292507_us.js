5d51c0b0706d50a62ec9b8c59aa8a11d9d2412c0
GitHub Action <action@github.com>
2025-01-23 02:57:39 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.xingin.discover",
   "id": 741292507,
   "name": "rednote - share, connect, love",
-  "version": "8.69.3",
+  "version": "8.69.4",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/b2/bc/8a/b2bc8a51-42a0-c73c-470f-4d90427cdbe9/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
