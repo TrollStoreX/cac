51cfc4feaa9515ac92d3c8277ae8f258dff8c81d
GitHub Action <action@github.com>
2025-02-03 01:57:40 +0000

    save caches

--- Library/Caches/com.nan.cac/1373567447_us.js
+++ Library/Caches/com.nan.cac/1373567447_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.ruikq.decar",
   "id": 1373567447,
   "name": "Loon",
-  "version": "3.2.5",
+  "version": "3.2.6",
   "price": "$5.99",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/f4/94/a5/f494a586-0b46-bfd4-b144-0d900abfc842/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.google.ios.youtube",
   "id": 544007664,
   "name": "YouTube: Watch, Listen, Stream",
-  "version": "20.04.1",
+  "version": "20.05.4",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/5c/b1/e8/5cb1e895-95b4-3e92-d215-e98b1b21ec4b/logo_youtube_2024_q4_color-0-1x_U007emarketing-0-0-0-6-0-0-0-85-220-0.png/100x100bb.jpg"
 }
