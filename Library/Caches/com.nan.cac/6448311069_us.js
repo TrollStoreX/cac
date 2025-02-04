a8decf97569eea61fd9b8cbb16025982afaea79b
GitHub Action <action@github.com>
2025-02-04 01:55:59 +0000

    save caches

--- Library/Caches/com.nan.cac/6448311069_us.js
+++ Library/Caches/com.nan.cac/6448311069_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.openai.chat",
   "id": 6448311069,
   "name": "ChatGPT",
-  "version": "1.2025.030",
+  "version": "1.2025.031",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/b0/a2/01/b0a20166-127b-76ae-f08c-eec5f8308295/AppIcon-0-0-1x_U007epad-0-1-85-220.png/100x100bb.jpg"
 }
