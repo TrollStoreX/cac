f4c1aacf71a49fdce047ca4f5b617c5871c9f237
GitHub Action <action@github.com>
2025-02-20 02:00:12 +0000

    save caches

--- Library/Caches/com.nan.cac/444934666_cn.js
+++ Library/Caches/com.nan.cac/444934666_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.tencent.mqq",
   "id": 444934666,
   "name": "QQ",
-  "version": "9.1.35",
+  "version": "9.1.50",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/fe/80/37/fe80378f-dfae-edcc-2d40-643181544075/AppIcon-1-0-1x_U007epad-0-1-0-sRGB-85-220-0.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/6448311069_us.js
+++ Library/Caches/com.nan.cac/6448311069_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.openai.chat",
   "id": 6448311069,
   "name": "ChatGPT",
-  "version": "1.2025.042",
+  "version": "1.2025.043",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b2/1c/ec/b21cece6-e0d4-0569-30fe-70934d6b71b3/AppIcon-0-0-1x_U007epad-0-0-0-1-0-85-220.png/100x100bb.jpg"
 }
