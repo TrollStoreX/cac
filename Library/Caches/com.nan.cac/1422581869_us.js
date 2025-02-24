81820d54ff0db3ffa98c14837ac9a42664b280b9
GitHub Action <action@github.com>
2025-02-24 08:40:34 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.coolapk.app",
   "id": 1422581869,
   "name": "酷安-分享美好科技生活",
-  "version": "15.0.3",
+  "version": 15.1,
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/54/f3/38/54f338ce-fc5c-f7a2-d50b-70ba2bca6884/AppIcon-0-0-1x_U007emarketing-0-11-0-0-85-220.png/100x100bb.jpg"
 }
