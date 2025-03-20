429e30b228dc145992545948bd0ff6fef81e1445
GitHub Action <action@github.com>
2025-03-20 17:35:06 +0000

    save caches

--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -5,5 +5,5 @@
   "name": "Instagram",
   "version": "372.2.0",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/72/78/1a/72781a2c-16a5-695c-45ee-b467bc2fdf31/Prod-0-0-1x_U007ephone-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/95/d8/9d/95d89d9a-7eab-2743-0405-79023e5f697b/Prod-0-0-1x_U007ephone-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
