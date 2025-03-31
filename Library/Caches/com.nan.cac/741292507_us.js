e38707c6c22fbe3c929437c0a631e4b76974c094
GitHub Action <action@github.com>
2025-03-31 04:38:35 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -5,5 +5,5 @@
   "name": "rednote",
   "version": 8.77,
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/df/da/18/dfda1895-1ef0-c24e-b4aa-88586169f342/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/5e/d5/61/5ed56174-d8a4-cdb1-9942-c5a85ec66b97/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
