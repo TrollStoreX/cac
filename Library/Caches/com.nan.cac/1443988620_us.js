eab8136391e686a1f76951f8fe2ec661dc569c6a
GitHub Action <action@github.com>
2025-05-07 09:38:57 +0000

    save caches

--- Library/Caches/com.nan.cac/1443988620_us.js
+++ Library/Caches/com.nan.cac/1443988620_us.js
@@ -5,5 +5,5 @@
   "name": "Quantumult X",
   "version": "1.5.1",
   "price": "$9.99",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/50/79/7c/50797c36-f545-27c9-c354-59a158ffe1d8/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/18/2f/54/182f54c4-a298-aa87-e242-7913eb018911/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
