5f0edbd6eea64b531e5d6b60b2711be9f1c3aca3
GitHub Action <action@github.com>
2025-05-15 18:59:59 +0000

    save caches

--- Library/Caches/com.nan.cac/324684580_us.js
+++ Library/Caches/com.nan.cac/324684580_us.js
@@ -5,5 +5,5 @@
   "name": "Spotify: Music and Podcasts",
   "version": "9.0.42",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b9/16/a4/b916a42f-40e9-4ffe-034d-edee70f200ad/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/5e/8d/ba/5e8dbab3-54a1-588a-d5f4-3702a16fd0ce/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
