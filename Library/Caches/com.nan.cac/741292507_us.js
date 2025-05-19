a68a83a6f81d4725e9bc298418338d4b36e12b26
GitHub Action <action@github.com>
2025-05-19 07:36:37 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.xingin.discover",
   "id": 741292507,
   "name": "rednote",
-  "version": 8.83,
+  "version": 8.84,
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/3d/e9/e9/3de9e964-6176-2949-e506-b2f9d248068b/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
