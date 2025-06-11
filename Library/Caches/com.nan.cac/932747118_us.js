b20484544b5c1ac5be269eaeeb885d0d93d92103
GitHub Action <action@github.com>
2025-06-11 04:46:15 +0000

    save caches

--- Library/Caches/com.nan.cac/932747118_us.js
+++ Library/Caches/com.nan.cac/932747118_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.liguangming.Shadowrocket",
   "id": 932747118,
   "name": "Shadowrocket",
-  "version": "2.2.66",
+  "version": "2.2.67",
   "price": "$2.99",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b6/90/60/b690609b-70f8-b613-83d7-a06242e66108/AppIcon-0-0-1x_U007epad-0-1-85-220.png/100x100bb.jpg"
 }
