1eda53fb70797f2ac7089ad597cf031c383d11a3
GitHub Action <action@github.com>
2025-06-16 18:47:39 +0000

    save caches

--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.burbn.instagram",
   "id": 389801252,
   "name": "Instagram",
-  "version": "384.0.0",
+  "version": "385.0.0",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b4/61/9d/b4619db8-f45e-955d-40d4-0115f41ce1ea/Prod-0-0-1x_U007ephone-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/6448311069_us.js
+++ Library/Caches/com.nan.cac/6448311069_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.openai.chat",
   "id": 6448311069,
   "name": "ChatGPT",
-  "version": "1.2025.155",
+  "version": "1.2025.161",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/7e/a3/5c/7ea35c4b-a591-f044-ea1e-1aa7932537d6/AppIcon-0-0-1x_U007epad-0-0-0-1-0-85-220.png/100x100bb.jpg"
 }
