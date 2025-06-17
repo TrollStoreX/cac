04d513707a8d9162569928d60ca40173a067cade
GitHub Action <action@github.com>
2025-06-17 21:54:05 +0000

    save caches

--- Library/Caches/com.nan.cac/1411932809_us.js
+++ Library/Caches/com.nan.cac/1411932809_us.js
@@ -5,5 +5,5 @@
   "name": "威锋-科技新闻&果粉互动交流社区",
   "version": "7.0.7",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b1/ca/fc/b1cafc57-46e2-57c8-e81a-8676e09b4298/AppIcon-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b7/27/67/b72767e8-9a87-b49e-33e7-c140068556b3/AppIcon-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.google.ios.youtube",
   "id": 544007664,
   "name": "YouTube",
-  "version": "20.23.3",
+  "version": "20.24.4",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/42/88/50/428850d1-1ff2-19a6-b691-f08e7b827d88/logo_youtube_2024_q4_color-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220-0.png/100x100bb.jpg"
 }
