c8324a7aa9517b1d475a866c49d5c154c6b940ed
GitHub Action <action@github.com>
2025-06-19 04:01:51 +0000

    save caches

--- Library/Caches/com.nan.cac/1534690075_us.js
+++ Library/Caches/com.nan.cac/1534690075_us.js
@@ -3,7 +3,7 @@
   "bundle": "tech.baye.placeholder",
   "id": 1534690075,
   "name": "DAMA - Auto Redact Privacy",
-  "version": 1.26,
+  "version": "1.26.1",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/da/a9/1b/daa91b96-6553-8e2e-30cf-aaf0d178210f/AppIcon-0-1x_U007epad-0-1-0-85-220-0.png/100x100bb.jpg"
 }
