72517b4dd36714e19525e28dc9488bb9c4dac39d
GitHub Action <action@github.com>
2025-07-07 18:47:45 +0000

    save caches

--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.burbn.instagram",
   "id": 389801252,
   "name": "Instagram",
-  "version": "387.0.0",
+  "version": "388.0.0",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/45/76/a8/4576a894-cc00-7cdb-a8a4-9f6a6aee2e0c/Prod-0-0-1x_U007ephone-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -5,5 +5,5 @@
   "name": "rednote",
   "version": 8.91,
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d1/54/71/d1547165-ff87-9322-a237-a99efc7fd673/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d9/4d/57/d94d57f1-cd80-4d84-7e94-f96b3f4890a2/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
