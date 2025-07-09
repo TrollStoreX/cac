504718b42a302415dceb3d685eda9df017e18ed9
GitHub Action <action@github.com>
2025-07-09 04:51:34 +0000

    save caches

--- Library/Caches/com.nan.cac/587366035_us.js
+++ Library/Caches/com.nan.cac/587366035_us.js
@@ -5,5 +5,5 @@
   "name": "Picsart AI Photo Editor, Video",
   "version": "27.8.4",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/6b/d3/51/6bd3517d-b8e7-97ab-a281-de34d1511c40/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b8/23/e1/b823e146-449f-8a79-30ae-5a191fafe081/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/835599320_us.js
+++ Library/Caches/com.nan.cac/835599320_us.js
@@ -5,5 +5,5 @@
   "name": "TikTok - Videos, Shop & LIVE",
   "version": "40.7.0",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/41/9d/15/419d1573-e57b-4018-cb40-d6845037a8e8/AppIcon_TikTok-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/db/ee/7d/dbee7d5b-9dde-6c2e-b5e8-d3fbe4023fbe/AppIcon_TikTok-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
