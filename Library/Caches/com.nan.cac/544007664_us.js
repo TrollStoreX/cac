d0e3fbe7dd883a65238d2edd4b30adf0d58db413
GitHub Action <action@github.com>
2025-07-14 05:00:03 +0000

    save caches

--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -5,5 +5,5 @@
   "name": "YouTube",
   "version": "20.28.2",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/b5/30/98/b5309846-02e7-a3bc-e412-64ca57ce2f41/logo_youtube_2024_q4_color-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/0e/28/24/0e282411-f2e0-4b46-65e5-c6131964cebb/logo_youtube_2024_q4_color-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220-0.png/100x100bb.jpg"
 }
