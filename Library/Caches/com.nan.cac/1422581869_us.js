27b5d3cdfe2deb904b5c0b4bf44621be56aa7eb2
GitHub Action <action@github.com>
2025-08-01 04:26:54 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.coolapk.app",
   "id": 1422581869,
   "name": "酷安-分享美好科技生活",
-  "version": 15.5,
+  "version": "15.5.1",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/46/bd/25/46bd2563-3384-2b8e-f9d5-8a5428a63ff6/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.xingin.discover",
   "id": 741292507,
   "name": "rednote",
-  "version": 8.94,
+  "version": "8.94.2",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/ad/c9/b6/adc9b638-2cad-c34c-7595-652e56f01c63/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
