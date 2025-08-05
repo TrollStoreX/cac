666c4cce41151af0ecc66a864e1782fdc3ad70a5
GitHub Action <action@github.com>
2025-08-05 21:57:48 +0000

    save caches

--- Library/Caches/com.nan.cac/835599320_us.js
+++ Library/Caches/com.nan.cac/835599320_us.js
@@ -5,5 +5,5 @@
   "name": "TikTok - Videos, Shop & LIVE",
   "version": "40.9.0",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/5c/86/31/5c8631fb-7d0f-32d9-a7c3-14db00368985/AppIcon_TikTok-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/f7/e1/f2/f7e1f251-1eb0-a5b1-e3f7-f38d19560424/AppIcon_TikTok-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
