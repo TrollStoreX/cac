9905981a26e0ab44e502bdf16ae245059dd0ce65
GitHub Action <action@github.com>
2025-08-05 10:02:15 +0000

    save caches

--- Library/Caches/com.nan.cac/388627783_cn.js
+++ Library/Caches/com.nan.cac/388627783_cn.js
@@ -5,5 +5,5 @@
   "name": "扫描全能王-手机扫描仪 & PDF图片转文字",
   "version": "6.93.5",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/38/cd/9e/38cd9e0b-1bbc-d8a4-cbe6-ade193108fbf/AppIconLite-0-1x_U007ephone-0-6-0-0-P3-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/a4/51/e8/a451e88c-1c61-fe9f-5741-0116b3ff84ef/AppIconLite-0-1x_U007ephone-0-6-0-0-P3-85-220-0.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -5,5 +5,5 @@
   "name": "rednote",
   "version": 8.949999999999999,
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/3f/2b/8e/3f2b8ea2-c0e5-0b26-c1bf-569052fdaea0/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/33/20/1f/33201fba-637d-54d0-ed4d-9e9be1ed9923/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
