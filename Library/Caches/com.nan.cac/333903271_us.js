20d4443478002af880321ad9a00f3fc0c771bae5
GitHub Action <action@github.com>
2025-08-06 15:44:01 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.atebits.Tweetie2",
   "id": 333903271,
   "name": "X",
-  "version": "11.11.1",
+  "version": "11.11.5",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/f5/ce/89/f5ce89f3-86eb-9a33-57a7-7fe84e54698a/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
 }
