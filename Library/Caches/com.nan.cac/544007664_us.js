43d9c98b1c9c607654304d18f81f6f1729eac626
GitHub Action <action@github.com>
2025-08-14 11:55:40 +0000

    save caches

--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.google.ios.youtube",
   "id": 544007664,
   "name": "YouTube",
-  "version": "20.32.4",
+  "version": "20.32.5",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/9b/64/83/9b6483d8-382a-8c0f-7d74-a12bc6ed5c0d/logo_youtube_2024_q4_color-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220-0.png/100x100bb.jpg"
 }
