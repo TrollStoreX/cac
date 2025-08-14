022d8b29195bd97d760f6cbcee1cd129f3f88d0e
GitHub Action <action@github.com>
2025-08-14 09:42:46 +0000

    save caches

--- Library/Caches/com.nan.cac/388627783_cn.js
+++ Library/Caches/com.nan.cac/388627783_cn.js
@@ -5,5 +5,5 @@
   "name": "扫描全能王-手机扫描仪 & PDF图片转文字",
   "version": "6.94.0",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/38/cd/9e/38cd9e0b-1bbc-d8a4-cbe6-ade193108fbf/AppIconLite-0-1x_U007ephone-0-6-0-0-P3-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/36/b7/8c/36b78cb2-d7e0-2111-2ac0-a06cd184bdc3/AppIconLite-0-1x_U007ephone-0-6-0-0-P3-85-220-0.png/100x100bb.jpg"
 }
