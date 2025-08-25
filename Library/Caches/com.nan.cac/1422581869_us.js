f8931d13bee82ce5cfe097ca31c999b0f99cca60
GitHub Action <action@github.com>
2025-08-25 15:56:28 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -5,5 +5,5 @@
   "name": "酷安-分享美好科技生活",
   "version": "15.5.3",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/ca/2a/7f/ca2a7f1e-ccd3-0c8c-a9db-a1228d92b310/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/3c/89/87/3c8987ad-56b8-978c-c743-3e28ee84b28d/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
 }
