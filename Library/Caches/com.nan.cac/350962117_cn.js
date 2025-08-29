1c85fbb571b77597f836a12499bbaa26563a6afb
GitHub Action <action@github.com>
2025-08-29 21:32:52 +0000

    save caches

--- Library/Caches/com.nan.cac/350962117_cn.js
+++ Library/Caches/com.nan.cac/350962117_cn.js
@@ -5,5 +5,5 @@
   "name": "微博",
   "version": "15.8.4",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/1a/22/d9/1a22d943-5e3f-6b42-c45e-5cf41e01f992/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/53/f0/2e/53f02ea2-a93c-6d30-6566-da5ffd74bea2/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
