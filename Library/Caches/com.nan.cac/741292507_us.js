d65ea3e938c8de8bd997694200d8ec47368cb5e6
GitHub Action <action@github.com>
2025-09-18 05:38:00 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -5,5 +5,5 @@
   "name": "小红书 – 你的生活兴趣社区",
   "version": "9.1.2",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/69/43/3d/69433d1f-51dd-0fef-f12f-803893aafce9/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/cf/8c/0c/cf8c0c00-10d0-8384-d19c-57b6507b8cc1/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
