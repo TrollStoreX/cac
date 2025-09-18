dafdee70abd4d3051c1097de3ed54b0674138964
GitHub Action <action@github.com>
2025-09-18 08:59:33 +0000

    save caches

--- Library/Caches/com.nan.cac/1411932809_us.js
+++ Library/Caches/com.nan.cac/1411932809_us.js
@@ -4,6 +4,6 @@
   "id": 1411932809,
   "name": "威锋-科技新闻&果粉互动交流社区",
   "version": "7.0.8",
-  "price": "Free",
+  "price": null,
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/26/5e/07/265e071d-2a10-c215-4020-92f710f82ed1/AppIcon-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
 }
