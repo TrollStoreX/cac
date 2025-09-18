9314d9d5aedda2aee45b5452f1ba50139cae1bc1
GitHub Action <action@github.com>
2025-09-18 13:42:48 +0000

    save caches

--- Library/Caches/com.nan.cac/1534690075_us.js
+++ Library/Caches/com.nan.cac/1534690075_us.js
@@ -5,5 +5,5 @@
   "name": "DAMA - Auto Redact Privacy",
   "version": "1.29.0",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/e2/72/04/e2720457-c057-9478-0db4-895904321fd7/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c0/16/fa/c016fac4-01fb-894f-214f-137b5d4b6026/AppIcon-0-1x_U007epad-0-1-0-85-220-0.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/324684580_us.js
+++ Library/Caches/com.nan.cac/324684580_us.js
@@ -5,5 +5,5 @@
   "name": "Spotify: Music and Podcasts",
   "version": "9.0.80",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/96/cb/36/96cb367e-a485-4abb-a334-0641e9dc0085/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/12/80/89/12808909-c9eb-d4f3-7e12-3df2b114b614/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
