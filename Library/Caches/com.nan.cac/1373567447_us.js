2ec5bdff5633e47649349c6e6173709bfad57918
GitHub Action <action@github.com>
2025-09-18 11:53:04 +0000

    save caches

--- Library/Caches/com.nan.cac/1373567447_us.js
+++ Library/Caches/com.nan.cac/1373567447_us.js
@@ -5,5 +5,5 @@
   "name": "Loon",
   "version": "3.3.4",
   "price": "$7.99",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/64/97/0e/64970e65-495d-fe9c-faf5-fddf5662d734/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/66/24/78/66247874-cfac-2fd8-28e1-0e3cf6cf9ea3/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
