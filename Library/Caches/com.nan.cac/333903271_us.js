750d1c2df4dc54535a53c7711eeff4ae03b6e973
GitHub Action <action@github.com>
2025-09-25 23:53:24 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -5,5 +5,5 @@
   "name": "X",
   "version": "11.25.1",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/3f/7a/7a/3f7a7a41-556e-6c91-87cc-6e845a82f3c5/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/fa/68/58/fa685858-4ff7-1801-73dd-cbe02087f486/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -5,5 +5,5 @@
   "name": "小红书 – 你的生活兴趣社区",
   "version": "9.2.4",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/4e/fb/96/4efb96e8-da89-6970-3ef3-d6d94d137ed7/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/1b/32/54/1b32543c-2bbb-7cff-e973-ab262d55e065/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
