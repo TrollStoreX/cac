a1ae331c79cf35d2de42e3700f0c413904453750
GitHub Action <action@github.com>
2025-10-03 14:54:38 +0000

    save caches

--- Library/Caches/com.nan.cac/444934666_cn.js
+++ Library/Caches/com.nan.cac/444934666_cn.js
@@ -4,6 +4,6 @@
   "id": 444934666,
   "name": "QQ",
   "version": "9.2.20",
-  "price": null,
+  "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/05/b8/6205b8b0-cd35-1356-7480-848204e12821/AppIcon-1-0-1x_U007epad-0-1-0-sRGB-85-220-0.png/100x100bb.jpg"
 }
