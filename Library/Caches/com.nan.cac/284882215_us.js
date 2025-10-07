c37c31489095a8975e27711bb028915edb046a8d
GitHub Action <action@github.com>
2025-10-07 14:40:58 +0000

    save caches

--- Library/Caches/com.nan.cac/284882215_us.js
+++ Library/Caches/com.nan.cac/284882215_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.facebook.Facebook",
   "id": 284882215,
   "name": "Facebook",
-  "version": "533.0.0",
+  "version": "534.0.0",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/78/ff/c0/78ffc0c9-8baa-2908-da5a-0bf06065e3b6/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/324684580_us.js
+++ Library/Caches/com.nan.cac/324684580_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.spotify.client",
   "id": 324684580,
   "name": "Spotify: Music and Podcasts",
-  "version": "9.0.84",
+  "version": "9.0.86",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/30/a8/c3/30a8c3fe-71c5-6646-529a-9bb8edc2b543/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
