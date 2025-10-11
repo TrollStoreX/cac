eb6b299b1bffc66fa87db3f7e30011c68d87de23
GitHub Action <action@github.com>
2025-10-11 07:52:42 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.xingin.discover",
   "id": 741292507,
   "name": "小红书 – 你的生活兴趣社区",
-  "version": "9.3.2",
+  "version": 9.4,
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/14/64/6c/14646c3a-6546-685b-6cfb-27c88c4a1d99/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
