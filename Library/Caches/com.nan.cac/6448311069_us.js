12f3ec1b961ff8cf464ed25d4c472d1d3b7e0ff0
GitHub Action <action@github.com>
2025-10-14 20:54:48 +0000

    save caches

--- Library/Caches/com.nan.cac/6448311069_us.js
+++ Library/Caches/com.nan.cac/6448311069_us.js
@@ -5,5 +5,5 @@
   "name": "ChatGPT",
   "version": "1.2025.280",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/15/4c/89/154c8910-0df4-3e9b-1069-eaebe818848e/AppIcon-0-0-1x_U007epad-0-0-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/57/cf/44/57cf442e-146b-0faf-a038-bb5baa6c5f02/AppIcon-0-0-1x_U007epad-0-0-0-1-0-85-220.png/100x100bb.jpg"
 }
