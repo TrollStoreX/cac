288ddb5fa32c93865805a9f24f65a64496b0f811
GitHub Action <action@github.com>
2025-10-19 20:53:44 +0000

    save caches

--- Library/Caches/com.nan.cac/1443988620_us.js
+++ Library/Caches/com.nan.cac/1443988620_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.crossutility.quantumult-x",
   "id": 1443988620,
   "name": "Quantumult X",
-  "version": "1.5.3",
+  "version": "1.5.4",
   "price": "$9.99",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/29/33/18/29331864-0814-5f10-ef88-ce4a1f96e741/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
