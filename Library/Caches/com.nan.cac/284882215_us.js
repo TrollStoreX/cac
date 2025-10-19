a1bb94bc997342e3d5bf644374c5f70f3ccf711e
GitHub Action <action@github.com>
2025-10-19 11:29:18 +0000

    save caches

--- Library/Caches/com.nan.cac/284882215_us.js
+++ Library/Caches/com.nan.cac/284882215_us.js
@@ -5,5 +5,5 @@
   "name": "Facebook",
   "version": "535.0.0",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/dc/36/a7/dc36a72d-91cc-f986-8491-dcce2d91c1ab/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/db/f7/40/dbf74089-54d3-a5a1-67ea-ba9bcefa8481/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
