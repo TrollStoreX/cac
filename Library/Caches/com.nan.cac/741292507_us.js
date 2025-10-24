8a836e90bc2222c249b0a32414904367aaf7fc78
GitHub Action <action@github.com>
2025-10-24 14:59:31 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -2,8 +2,8 @@
   "status": 0,
   "bundle": "com.xingin.discover",
   "id": 741292507,
-  "name": "rednote",
+  "name": "小红书 – 你的生活兴趣社区",
   "version": 9.6,
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/59/ff/23/59ff23c2-9df8-8d6e-78e0-955cb7270e22/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/24/1a/7d/241a7d2b-49c3-2bfb-3f9a-9e17fc0b530d/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
