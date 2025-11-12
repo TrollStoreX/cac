4f1ef5d468dab6d728304d0f7da8328d0c36bccd
trollstorex <59131675+TrollStoreX@users.noreply.github.com>
2025-11-12 15:18:22 +0800

    Update 1142110895_cn.js

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -3,7 +3,7 @@
   "id": 1142110895,
   "bundle": "com.ss.iphone.ugc.Aweme",
   "name": "抖音",
-  "version": "36.5.0",
+  "version": "36.2.0",
   "version_id": 879451265,
   "version_release_timestamp": "2025-11-05T10:06:20Z",
   "price": null,
