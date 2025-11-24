6a025113592e2cf2433e98ea38b12bdc5f40986d
GitHub Action <action@github.com>
2025-11-24 02:26:56 +0000

    save caches

--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -3,9 +3,9 @@
   "id": 544007664,
   "bundle": "com.google.ios.youtube",
   "name": "YouTube",
-  "version": "20.46.3",
-  "version_id": 879927013,
-  "version_release_timestamp": "2025-11-20T17:22:10Z",
+  "version": "20.47.3",
+  "version_id": 879987358,
+  "version_release_timestamp": "2025-11-24T00:06:27Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/07/6b/c1/076bc13d-b5ca-3f0e-0c40-9e208ff3f769/logo_youtube_2024_q4_color-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/31/d8/8a/31d88a82-68df-84da-3686-2d2ff511375c/logo_youtube_2024_q4_color-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220-0.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/587366035_us.js
+++ Library/Caches/com.nan.cac/587366035_us.js
@@ -3,9 +3,9 @@
   "id": 587366035,
   "bundle": "com.picsart.studio",
   "name": "Picsart AI Photo Editor, Video",
-  "version": "28.8.2",
-  "version_id": 879672245,
-  "version_release_timestamp": "2025-11-19T20:42:40Z",
+  "version": "28.8.4",
+  "version_id": 879983262,
+  "version_release_timestamp": "2025-11-24T01:36:52Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/8c/33/d7/8c33d7e7-4737-5fee-1b58-6d25b47fd0cf/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/be/e6/6a/bee66a40-62e0-89e3-39be-bb39b0947a17/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
 }
