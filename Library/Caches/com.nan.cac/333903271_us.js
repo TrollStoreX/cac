b4ec75daf8f7900072db7b534b088263295a61bb
GitHub Action <action@github.com>
2025-11-25 10:43:05 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -3,9 +3,9 @@
   "id": 333903271,
   "bundle": "com.atebits.Tweetie2",
   "name": "X",
-  "version": "11.42.1",
-  "version_id": 880020853,
-  "version_release_timestamp": "2025-11-24T03:35:31Z",
+  "version": "11.42.2",
+  "version_id": 880061735,
+  "version_release_timestamp": "2025-11-25T09:54:08Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/38/d6/6238d630-21af-35af-23cd-d94acd15a0f4/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d4/41/5c/d4415c28-e833-c08a-5007-eef5a9208e2f/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/835599320_us.js
+++ Library/Caches/com.nan.cac/835599320_us.js
@@ -3,9 +3,9 @@
   "id": 835599320,
   "bundle": "com.zhiliaoapp.musically",
   "name": "TikTok - Videos, Shop & LIVE",
-  "version": "42.6.0",
-  "version_id": 879763410,
-  "version_release_timestamp": "2025-11-18T22:18:20Z",
+  "version": "42.7.0",
+  "version_id": 879962066,
+  "version_release_timestamp": "2025-11-25T09:13:15Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c6/35/45/c6354534-2fa9-071b-801c-d9d03c256d23/AppIcon_TikTok-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/e9/84/5a/e9845a37-4ff9-02de-4aad-4963348c12c6/AppIcon_TikTok-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
