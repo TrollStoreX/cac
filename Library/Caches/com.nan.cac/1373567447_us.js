63c5500b527fb8ffdde8b7909eaf135706b785b2
GitHub Action <action@github.com>
2025-11-27 02:12:17 +0000

    save caches

--- Library/Caches/com.nan.cac/1373567447_us.js
+++ Library/Caches/com.nan.cac/1373567447_us.js
@@ -3,9 +3,9 @@
   "id": 1373567447,
   "bundle": "com.ruikq.decar",
   "name": "Loon",
-  "version": "3.3.5",
-  "version_id": 878858008,
-  "version_release_timestamp": "2025-10-13T11:06:34Z",
+  "version": "3.3.6",
+  "version_id": 880096345,
+  "version_release_timestamp": "2025-11-27T01:34:06Z",
   "price": "USD 7.99",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/a1/5e/79/a15e79b1-9e6a-9714-c9f8-232e1b09f8f7/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/53/d4/fc/53d4fcf3-af0e-08bf-4119-ce8135de9b85/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -3,9 +3,9 @@
   "id": 333903271,
   "bundle": "com.atebits.Tweetie2",
   "name": "X",
-  "version": 11.43,
-  "version_id": 880090005,
-  "version_release_timestamp": "2025-11-26T04:21:46Z",
+  "version": "11.43.1",
+  "version_id": 880112965,
+  "version_release_timestamp": "2025-11-26T23:27:56Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/46/dd/d3/46ddd390-1a51-1a0d-e795-82ab75f3e7a8/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/55/2a/69/552a69b6-4de4-2097-2ae5-a7662ffc2d7d/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
 }
