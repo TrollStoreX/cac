352e7f27b82cbc59fef21c3131e2c088b03f5218
GitHub Action <action@github.com>
2025-12-02 14:43:59 +0000

    save caches

--- Library/Caches/com.nan.cac/284882215_us.js
+++ Library/Caches/com.nan.cac/284882215_us.js
@@ -3,9 +3,9 @@
   "id": 284882215,
   "bundle": "com.facebook.Facebook",
   "name": "Facebook",
-  "version": "540.0.0",
-  "version_id": 879692894,
-  "version_release_timestamp": "2025-11-22T12:57:53Z",
+  "version": "541.0.0",
+  "version_id": 879998243,
+  "version_release_timestamp": "2025-12-02T13:56:52Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/4f/24/c7/4f24c792-ebb1-b84a-5148-30d4a9bbe686/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/2f/f2/c4/2ff2c44d-a8a3-0923-1c56-52c55b62bca6/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
