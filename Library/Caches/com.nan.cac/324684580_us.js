f7494d331e86cc24cfc9720cd6ad61e671717273
GitHub Action <action@github.com>
2025-12-04 09:02:56 +0000

    save caches

--- Library/Caches/com.nan.cac/324684580_us.js
+++ Library/Caches/com.nan.cac/324684580_us.js
@@ -3,9 +3,9 @@
   "id": 324684580,
   "bundle": "com.spotify.client",
   "name": "Spotify: Music and Podcasts",
-  "version": "9.1.0",
-  "version_id": 879946042,
-  "version_release_timestamp": "2025-11-27T13:56:47Z",
+  "version": "9.1.2",
+  "version_id": 880147872,
+  "version_release_timestamp": "2025-12-04T07:20:30Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/ad/57/03/ad5703d9-baa3-e836-bddd-a8e19447ca42/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/a4/b4/d7/a4b4d72d-4675-b950-344d-b19fa35284f6/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
