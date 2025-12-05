cf732cabfd97eb34a8fad7e14b096b39ceb3a857
GitHub Action <action@github.com>
2025-12-05 15:39:22 +0000

    save caches

--- Library/Caches/com.nan.cac/944665061_us.js
+++ Library/Caches/com.nan.cac/944665061_us.js
@@ -3,9 +3,9 @@
   "id": 944665061,
   "bundle": "com.loveyouchenapps.knockout",
   "name": "ProKnockOut-Cut Paste Photos",
-  "version": 7.82,
-  "version_id": 879485237,
-  "version_release_timestamp": "2025-11-06T11:46:52Z",
+  "version": 7.83,
+  "version_id": 880293791,
+  "version_release_timestamp": "2025-12-05T14:36:39Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/59/8d/9f/598d9ff3-7158-1333-84d8-05e5df96305c/AppIcon-0-0-1x_U007epad-0-1-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/5a/e2/3c/5ae23c31-63a0-e0ae-6643-b58bd33140cf/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
