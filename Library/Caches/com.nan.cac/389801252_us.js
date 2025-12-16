6c72283b244a5efb03fa32e546da86273a58e6b5
GitHub Action <action@github.com>
2025-12-16 16:49:51 +0000

    save caches

--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,9 +3,9 @@
   "id": 389801252,
   "bundle": "com.burbn.instagram",
   "name": "Instagram",
-  "version": "409.1.0",
-  "version_id": 880460733,
-  "version_release_timestamp": "2025-12-10T14:49:56Z",
+  "version": "410.0.0",
+  "version_id": 880525084,
+  "version_release_timestamp": "2025-12-16T16:03:50Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/6b/b5/89/6bb589b5-2b76-bff0-be4d-fea99d1b52db/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/bb/05/21/bb052141-2812-3294-5b22-b39e6134ce11/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
