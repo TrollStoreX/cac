3ba2fbad764dbeb89f925dd1e620fba770ed0d20
GitHub Action <action@github.com>
2025-12-22 04:02:34 +0000

    save caches

--- Library/Caches/com.nan.cac/388627783_cn.js
+++ Library/Caches/com.nan.cac/388627783_cn.js
@@ -3,9 +3,9 @@
   "id": 388627783,
   "bundle": "com.intsig.CamScannerLite",
   "name": "扫描全能王-手机扫描仪 & PDF图片转文字",
-  "version": "7.8.0",
-  "version_id": 880560194,
-  "version_release_timestamp": "2025-12-15T02:52:45Z",
+  "version": "7.8.5",
+  "version_id": 880809641,
+  "version_release_timestamp": "2025-12-22T03:23:04Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/f4/2d/5c/f42d5cc1-8100-bd7f-258f-6d2d15de47fb/AppIconLite-0-1x_U007ephone-0-6-0-0-sRGB-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/4d/2b/d9/4d2bd979-21f4-9922-2f75-71414e69d499/AppIconLite-0-1x_U007ephone-0-6-0-0-sRGB-85-220-0.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/932747118_us.js
+++ Library/Caches/com.nan.cac/932747118_us.js
@@ -3,9 +3,9 @@
   "id": 932747118,
   "bundle": "com.liguangming.Shadowrocket",
   "name": "Shadowrocket",
-  "version": "2.2.75",
-  "version_id": 880478929,
-  "version_release_timestamp": "2025-12-10T07:41:29Z",
+  "version": "2.2.76",
+  "version_id": 880575458,
+  "version_release_timestamp": "2025-12-22T02:41:58Z",
   "price": "USD 2.99",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/b9/d6/69/b9d6690e-0f79-a30f-6064-38730c0e1dca/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/48/a3/80/48a38037-de9b-c900-0489-bea9c94d1c48/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
