06707e19cb627959057cc9aea61fcb18a3c5dcc5
GitHub Action <action@github.com>
2026-01-13 03:57:24 +0000

    save caches

--- Library/Caches/com.nan.cac/350962117_cn.js
+++ Library/Caches/com.nan.cac/350962117_cn.js
@@ -3,9 +3,9 @@
   "id": 350962117,
   "bundle": "com.sina.weibo",
   "name": "微博",
-  "version": "15.12.4",
-  "version_id": 881096101,
-  "version_release_timestamp": "2026-01-07T02:19:39Z",
+  "version": "15.12.5",
+  "version_id": 881265527,
+  "version_release_timestamp": "2026-01-13T01:59:06Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c3/36/c0/c336c0da-3524-aa51-a005-161395a469ef/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/e6/2e/af/e62eaf92-9884-c941-809d-91a73f161cf1/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/736536022_us.js
+++ Library/Caches/com.nan.cac/736536022_us.js
@@ -3,9 +3,9 @@
   "id": 736536022,
   "bundle": "tv.danmaku.bilianime",
   "name": "bilibili - All Your Fav Videos",
-  "version": "8.78.0",
-  "version_id": 881190128,
-  "version_release_timestamp": "2026-01-07T03:10:11Z",
+  "version": "8.79.0",
+  "version_id": 881343455,
+  "version_release_timestamp": "2026-01-13T03:07:25Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/71/e2/a7/71e2a7b6-5cfe-d4e5-f63d-96dd8b2242f2/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/40/28/06/40280649-94f2-92e7-a582-5483897446eb/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
