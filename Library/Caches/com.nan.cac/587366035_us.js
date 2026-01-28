53bf51733147451342709e420c45983d7d225319
GitHub Action <action@github.com>
2026-01-28 18:58:52 +0000

    save caches

--- Library/Caches/com.nan.cac/587366035_us.js
+++ Library/Caches/com.nan.cac/587366035_us.js
@@ -3,9 +3,9 @@
   "id": 587366035,
   "bundle": "com.picsart.studio",
   "name": "Picsart AI Photo Editor, Video",
-  "version": "29.1.2",
-  "version_id": 880912588,
-  "version_release_timestamp": "2025-12-23T07:47:45Z",
+  "version": "29.2.2",
+  "version_id": 881273141,
+  "version_release_timestamp": "2026-01-28T17:53:56Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/cf/8c/37/cf8c3798-8c5a-d06b-3f19-be42ce48cf4d/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/e4/d1/01/e4d1014b-5e0c-a312-76c8-0b0f24ea48a0/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
 }
