166a6ebacfc8a37010386ca605f40b6918191727
GitHub Action <action@github.com>
2026-02-04 15:02:57 +0000

    save caches

--- Library/Caches/com.nan.cac/1136220934_us.js
+++ Library/Caches/com.nan.cac/1136220934_us.js
@@ -3,9 +3,9 @@
   "id": 1136220934,
   "bundle": "com.firecore.infuse",
   "name": "Infuse",
-  "version": "8.3.6",
-  "version_id": 880809769,
-  "version_release_timestamp": "2026-01-20T18:29:07Z",
+  "version": "8.3.7",
+  "version_id": 881983344,
+  "version_release_timestamp": "2026-02-04T13:51:26Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/bc/82/9b/bc829b53-a913-3a1e-fa62-1672308614d2/AppIconLight-0-0-1x_U007emarketing-0-8-0-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/94/9a/c1/949ac1a9-7e37-b628-c330-1369cfb48b74/AppIcon-0-0-1x_U007epad-0-11-0-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,9 +3,9 @@
   "id": 389801252,
   "bundle": "com.burbn.instagram",
   "name": "Instagram",
-  "version": "415.0.0",
-  "version_id": 881857367,
-  "version_release_timestamp": "2026-02-02T16:31:54Z",
+  "version": "415.1.0",
+  "version_id": 882088152,
+  "version_release_timestamp": "2026-02-04T14:23:06Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/23/55/60/23556081-921f-d76b-29cf-93e28efda827/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/bb/df/a6/bbdfa6f7-261a-b26e-20c6-4412ffa72acd/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
