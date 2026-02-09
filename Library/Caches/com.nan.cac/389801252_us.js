0d635156544c74976acb39ea656f05b06a9641df
GitHub Action <action@github.com>
2026-02-09 15:15:36 +0000

    save caches

--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,9 +3,9 @@
   "id": 389801252,
   "bundle": "com.burbn.instagram",
   "name": "Instagram",
-  "version": "415.1.0",
-  "version_id": 882088152,
-  "version_release_timestamp": "2026-02-04T14:23:06Z",
+  "version": "416.0.0",
+  "version_id": 882157720,
+  "version_release_timestamp": "2026-02-09T13:28:28Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/bb/df/a6/bbdfa6f7-261a-b26e-20c6-4412ffa72acd/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/f4/32/42/f43242c5-a3ea-1ee1-49d0-5d63beb2e5f3/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/6471879502_us.js
+++ Library/Caches/com.nan.cac/6471879502_us.js
@@ -3,9 +3,9 @@
   "id": 6471879502,
   "bundle": "app.swiftgram.ios",
   "name": "Swiftgram: Telegram mod client",
-  "version": 12.3,
-  "version_id": 881367563,
-  "version_release_timestamp": "2026-01-11T20:11:07Z",
+  "version": "12.3.3",
+  "version_id": 882262506,
+  "version_release_timestamp": "2026-02-09T14:26:38Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/1b/54/65/1b546531-782a-330d-b6c2-2e429dff3876/Swiftgram-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/8b/6e/64/8b6e6440-197d-07a2-84d4-d3f589a2e700/Swiftgram-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
