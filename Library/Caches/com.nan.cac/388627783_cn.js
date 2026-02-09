696653268d13d8a2ba0b95147d977ef7dcc41fcd
GitHub Action <action@github.com>
2026-02-09 03:25:14 +0000

    save caches

--- Library/Caches/com.nan.cac/388627783_cn.js
+++ Library/Caches/com.nan.cac/388627783_cn.js
@@ -3,9 +3,9 @@
   "id": 388627783,
   "bundle": "com.intsig.CamScannerLite",
   "name": "扫描全能王-手机扫描仪 & PDF图片转文字",
-  "version": "7.11.0",
-  "version_id": 881739402,
-  "version_release_timestamp": "2026-01-26T02:22:36Z",
+  "version": "7.12.0",
+  "version_id": 882224701,
+  "version_release_timestamp": "2026-02-09T02:37:32Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/ca/7a/b9/ca7ab945-ccac-2dee-c332-dfb35109552e/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/80/7e/3f/807e3fde-b4c4-9f7b-6f44-d7d01702c254/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -3,9 +3,9 @@
   "id": 544007664,
   "bundle": "com.google.ios.youtube",
   "name": "YouTube",
-  "version": "21.05.3",
-  "version_id": 882017501,
-  "version_release_timestamp": "2026-02-02T00:04:32Z",
+  "version": "21.06.2",
+  "version_id": 882239427,
+  "version_release_timestamp": "2026-02-09T00:08:38Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/38/8e/37/388e377e-2f17-220b-5da4-400cd4911bef/logo_youtube_2024_q4_color-0-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/2a/5f/76/2a5f762d-602f-8078-8b4a-e0914cc81090/logo_youtube_2024_q4_color-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220-0.png/100x100bb.jpg"
 }
