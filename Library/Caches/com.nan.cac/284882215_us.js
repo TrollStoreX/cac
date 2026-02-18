2f77a73f64b933cc17b5964343e80566d92577f0
GitHub Action <action@github.com>
2026-02-18 22:57:23 +0000

    save caches

--- Library/Caches/com.nan.cac/284882215_us.js
+++ Library/Caches/com.nan.cac/284882215_us.js
@@ -3,9 +3,9 @@
   "id": 284882215,
   "bundle": "com.facebook.Facebook",
   "name": "Facebook",
-  "version": "548.0.0",
-  "version_id": 882137062,
-  "version_release_timestamp": "2026-02-11T12:55:16Z",
+  "version": "549.0.0",
+  "version_id": 882373723,
+  "version_release_timestamp": "2026-02-18T21:10:54Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/08/d5/f2/08d5f215-74b1-95bb-518c-2160d61c573e/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f5/05/7ff50514-1397-c02b-35fe-5eb1bcb68d10/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -3,9 +3,9 @@
   "id": 333903271,
   "bundle": "com.atebits.Tweetie2",
   "name": "X",
-  "version": 11.66,
-  "version_id": 882423072,
-  "version_release_timestamp": "2026-02-16T16:56:34Z",
+  "version": 11.67,
+  "version_id": 882558975,
+  "version_release_timestamp": "2026-02-18T21:37:11Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/fa/3c/09/fa3c0997-2abf-7130-5821-0eaf43bfe699/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/44/5c/2c/445c2cbe-2334-a8f7-4ebd-c733e653965b/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
 }
