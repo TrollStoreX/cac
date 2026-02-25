f7e4922c73914a1b9adb910125224ee3d1157578
GitHub Action <action@github.com>
2026-02-25 05:19:52 +0000

    save caches

--- Library/Caches/com.nan.cac/587366035_us.js
+++ Library/Caches/com.nan.cac/587366035_us.js
@@ -3,9 +3,9 @@
   "id": 587366035,
   "bundle": "com.picsart.studio",
   "name": "Picsart AI Photo Editor, Video",
-  "version": "29.4.2",
-  "version_id": 882642727,
-  "version_release_timestamp": "2026-02-23T05:16:00Z",
+  "version": "29.4.3",
+  "version_id": 882747409,
+  "version_release_timestamp": "2026-02-25T04:39:13Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/de/91/3d/de913d33-f0be-2425-1bb9-7fd928adcbce/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/9c/af/12/9caf1214-1f7d-bdbb-d37e-0487be5ebba0/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/736536022_us.js
+++ Library/Caches/com.nan.cac/736536022_us.js
@@ -3,9 +3,9 @@
   "id": 736536022,
   "bundle": "tv.danmaku.bilianime",
   "name": "bilibili - All Your Fav Videos",
-  "version": "8.83.0",
-  "version_id": 882223700,
-  "version_release_timestamp": "2026-02-10T04:20:02Z",
+  "version": "8.84.0",
+  "version_id": 882728879,
+  "version_release_timestamp": "2026-02-25T03:53:12Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/98/79/7c/98797ce0-3705-bb8c-aec7-ba96a05222ab/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/e3/7b/e3/e37be346-1148-836a-fd3e-d171cefbf979/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
