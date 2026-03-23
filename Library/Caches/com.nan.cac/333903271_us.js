941fdc84aed35b40e4571384af74216d31746699
GitHub Action <action@github.com>
2026-03-23 05:29:24 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -3,9 +3,9 @@
   "id": 333903271,
   "bundle": "com.atebits.Tweetie2",
   "name": "X",
-  "version": "11.75.2",
-  "version_id": 883569392,
-  "version_release_timestamp": "2026-03-20T10:33:09Z",
+  "version": 11.76,
+  "version_id": 883603258,
+  "version_release_timestamp": "2026-03-23T04:03:38Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c0/29/f9/c029f9b6-41f7-0a09-8091-ebc6689b4ff4/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/8f/9d/96/8f9d96ee-575c-bee4-6114-7144efd64241/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,9 +3,9 @@
   "id": 741292507,
   "bundle": "com.xingin.discover",
   "name": "rednote",
-  "version": "9.22.1",
-  "version_id": 883473311,
-  "version_release_timestamp": "2026-03-18T02:28:09Z",
+  "version": 9.23,
+  "version_id": 883581215,
+  "version_release_timestamp": "2026-03-23T02:59:39Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/85/cb/6a/85cb6a2a-dca1-f649-e61e-1da0706a00ce/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/cb/f0/74/cbf074c2-834c-359d-ff67-c2772dd0fe9f/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
