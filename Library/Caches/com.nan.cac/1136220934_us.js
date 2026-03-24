6dc11a759e3d028c497ec50104d2005005223539
GitHub Action <action@github.com>
2026-03-24 19:43:45 +0000

    save caches

--- Library/Caches/com.nan.cac/1136220934_us.js
+++ Library/Caches/com.nan.cac/1136220934_us.js
@@ -3,9 +3,9 @@
   "id": 1136220934,
   "bundle": "com.firecore.infuse",
   "name": "Infuse",
-  "version": "8.3.9",
-  "version_id": 882640355,
-  "version_release_timestamp": "2026-02-24T18:28:57Z",
+  "version": 8.4,
+  "version_id": 883319155,
+  "version_release_timestamp": "2026-03-24T18:56:51Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/be/15/1c/be151cfd-07e5-d26d-2488-2f95c3468beb/AppIcon-0-0-1x_U007epad-0-11-0-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/8f/28/82/8f2882af-99c7-42df-e9f4-fe78a9734d62/AppIcon-0-0-1x_U007epad-0-11-0-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/284882215_us.js
+++ Library/Caches/com.nan.cac/284882215_us.js
@@ -3,9 +3,9 @@
   "id": 284882215,
   "bundle": "com.facebook.Facebook",
   "name": "Facebook",
-  "version": "553.0.0",
+  "version": "554.0.0",
   "version_id": 883504368,
-  "version_release_timestamp": "2026-03-18T11:55:58Z",
+  "version_release_timestamp": "2026-03-24T18:29:59Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/6b/b0/f4/6bb0f459-a746-0827-6f85-8716ead92596/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/73/4c/c0/734cc096-c420-56a7-10a6-533055e59bb2/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
