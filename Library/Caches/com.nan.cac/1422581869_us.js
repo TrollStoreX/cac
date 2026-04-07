dfa35520005dae30b65bbaf0c594c78ce602c89a
GitHub Action <action@github.com>
2026-04-07 03:32:12 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,9 +3,9 @@
   "id": 1422581869,
   "bundle": "com.coolapk.app",
   "name": "酷安-分享美好科技生活",
-  "version": 16.1,
-  "version_id": 883652571,
-  "version_release_timestamp": "2026-03-24T01:22:34Z",
+  "version": 16.2,
+  "version_id": 884049960,
+  "version_release_timestamp": "2026-04-07T01:42:57Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/77/87/3f/77873f1b-8a1b-127b-4592-0629ed6092a1/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/17/83/68/178368ab-6f4f-1642-dd06-6e676c81530d/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/388627783_cn.js
+++ Library/Caches/com.nan.cac/388627783_cn.js
@@ -3,9 +3,9 @@
   "id": 388627783,
   "bundle": "com.intsig.CamScannerLite",
   "name": "扫描全能王-手机扫描仪 & PDF图片转文字",
-  "version": "7.14.5",
-  "version_id": 883792699,
-  "version_release_timestamp": "2026-03-30T05:18:42Z",
+  "version": "7.15.0",
+  "version_id": 884062130,
+  "version_release_timestamp": "2026-04-07T02:47:23Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/2b/ed/57/2bed573b-24f5-1ef9-1133-39ad4684993e/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/70/4d/32/704d32a1-070b-8369-39ba-7c87cecb7c10/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
 }
