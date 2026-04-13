30d85e78351b4c20d527a34f520f0097a2bbeec4
GitHub Action <action@github.com>
2026-04-13 17:23:37 +0000

    save caches

--- Library/Caches/com.nan.cac/6471879502_us.js
+++ Library/Caches/com.nan.cac/6471879502_us.js
@@ -3,9 +3,9 @@
   "id": 6471879502,
   "bundle": "app.swiftgram.ios",
   "name": "Swiftgram: Telegram mod client",
-  "version": 12.5,
-  "version_id": 883316220,
-  "version_release_timestamp": "2026-03-14T00:06:09Z",
+  "version": "12.6.2",
+  "version_id": 884431699,
+  "version_release_timestamp": "2026-04-13T16:02:23Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/c1/d5/fa/c1d5faba-f9f3-2b37-959b-4e69452bb301/Swiftgram-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/b8/8c/17/b88c17a8-31cc-e470-1ca1-1a3be2339234/Swiftgram-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
