f7f0f1cd0ebfc36fa7ce5ee8d9838de3c6887a4f
GitHub Action <action@github.com>
2026-04-20 18:46:30 +0000

    save caches

--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,9 +3,9 @@
   "id": 389801252,
   "bundle": "com.burbn.instagram",
   "name": "Instagram",
-  "version": "425.0.0",
-  "version_id": 884342636,
-  "version_release_timestamp": "2026-04-13T15:20:50Z",
+  "version": "426.0.0",
+  "version_id": 884467205,
+  "version_release_timestamp": "2026-04-20T17:48:35Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/18/73/96/187396d0-9ea0-cd65-83f6-42550506b3f3/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/be/34/9a/be349aba-d4dc-fd25-abf5-8724023cc7b9/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/6448311069_us.js
+++ Library/Caches/com.nan.cac/6448311069_us.js
@@ -3,9 +3,9 @@
   "id": 6448311069,
   "bundle": "com.openai.chat",
   "name": "ChatGPT",
-  "version": "1.2026.097",
-  "version_id": 884344662,
-  "version_release_timestamp": "2026-04-15T01:16:41Z",
+  "version": "1.2026.104",
+  "version_id": 884551145,
+  "version_release_timestamp": "2026-04-20T17:42:55Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/26/f1/83/26f18317-779a-5035-91e5-0f22dee98012/AppIcon-0-0-1x_U007epad-0-0-0-1-0-P3-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/1b/45/d6/1b45d659-d8bf-94b0-dfa6-0449fc565333/AppIcon-0-0-1x_U007epad-0-0-0-1-0-P3-85-220.png/100x100bb.jpg"
 }
