2fff007adabd074a7c33fc94cdd7d43c3910a75a
GitHub Action <action@github.com>
2026-04-29 07:56:41 +0000

    save caches

--- Library/Caches/com.nan.cac/324684580_us.js
+++ Library/Caches/com.nan.cac/324684580_us.js
@@ -3,9 +3,9 @@
   "id": 324684580,
   "bundle": "com.spotify.client",
   "name": "Spotify: Music and Podcasts",
-  "version": "9.1.42",
-  "version_id": 884765340,
-  "version_release_timestamp": "2026-04-23T07:21:18Z",
+  "version": "9.1.44",
+  "version_id": 885034385,
+  "version_release_timestamp": "2026-04-29T06:40:07Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/fc/90/9b/fc909b97-ceca-3bff-1112-196bd8a09f65/AppIcon-0-0-1x_U007epad-0-1-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/d8/fc/a5/d8fca55a-3797-96a0-1b01-c7ec6e0453e0/AppIcon-0-0-1x_U007epad-0-1-0-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/350962117_cn.js
+++ Library/Caches/com.nan.cac/350962117_cn.js
@@ -3,9 +3,9 @@
   "id": 350962117,
   "bundle": "com.sina.weibo",
   "name": "微博",
-  "version": "16.4.4",
-  "version_id": 884827532,
-  "version_release_timestamp": "2026-04-23T08:01:04Z",
+  "version": "16.5.0",
+  "version_id": 885049732,
+  "version_release_timestamp": "2026-04-29T06:15:36Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/55/34/2c/55342c64-920b-b5c2-ac4c-1ce7080dc729/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/33/27/99/332799ce-eea9-9058-79ae-e5728b142234/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
