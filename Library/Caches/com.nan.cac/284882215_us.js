7197a822c4a43efbd373a1addf017b49294d1784
GitHub Action <action@github.com>
2026-05-05 16:58:32 +0000

    save caches

--- Library/Caches/com.nan.cac/284882215_us.js
+++ Library/Caches/com.nan.cac/284882215_us.js
@@ -3,9 +3,9 @@
   "id": 284882215,
   "bundle": "com.facebook.Facebook",
   "name": "Facebook",
-  "version": 559,
-  "version_id": 884763588,
-  "version_release_timestamp": "2026-04-28T02:13:48Z",
+  "version": "560.0.0",
+  "version_id": 885007317,
+  "version_release_timestamp": "2026-05-05T14:18:24Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/87/de/10/87de107f-2391-d775-64a5-388a9292bc57/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/47/c9/8e/47c98ecd-d09e-c4a2-8ef2-a1db7c23292a/Icon-Production-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,9 +3,9 @@
   "id": 389801252,
   "bundle": "com.burbn.instagram",
   "name": "Instagram",
-  "version": "428.1.0",
-  "version_id": 885199357,
-  "version_release_timestamp": "2026-05-04T12:27:56Z",
+  "version": "428.2.0",
+  "version_id": 885243071,
+  "version_release_timestamp": "2026-05-05T15:02:20Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/ae/b0/d8/aeb0d8b0-580f-9e86-ecab-aac637e0b77b/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/81/11/b1/8111b1e6-eb8c-538c-5404-0587ce06699b/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
