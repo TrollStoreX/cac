c25313cdc05861c95b1e7ba6e0bef1ff12bfec77
GitHub Action <action@github.com>
2026-05-12 06:52:13 +0000

    save caches

--- Library/Caches/com.nan.cac/736536022_us.js
+++ Library/Caches/com.nan.cac/736536022_us.js
@@ -3,9 +3,9 @@
   "id": 736536022,
   "bundle": "tv.danmaku.bilianime",
   "name": "bilibili - All Your Fav Videos",
-  "version": "8.93.0",
-  "version_id": 885308823,
-  "version_release_timestamp": "2026-05-09T07:18:28Z",
+  "version": "8.94.0",
+  "version_id": 885520813,
+  "version_release_timestamp": "2026-05-12T04:05:37Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/d0/92/fb/d092fb46-eb6b-a087-c0c5-1cf056940140/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/47/06/58/47065892-d3bf-281b-aa0a-563f0503b070/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,9 +3,9 @@
   "id": 741292507,
   "bundle": "com.xingin.discover",
   "name": "rednote",
-  "version": 9.300000000000001,
-  "version_id": 885470274,
-  "version_release_timestamp": "2026-05-11T02:29:58Z",
+  "version": "9.30.1",
+  "version_id": 885536634,
+  "version_release_timestamp": "2026-05-12T03:44:37Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/b4/a7/ec/b4a7ecac-7772-bd02-59a3-639fdbe9e836/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/9b/8d/08/9b8d08da-e044-5c10-7cd0-52c689657c96/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
