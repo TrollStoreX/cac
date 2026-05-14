13fbde7ec922fc316fcf4e622bbb9ba7ab2b2521
GitHub Action <action@github.com>
2026-05-14 10:34:43 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,9 +3,9 @@
   "id": 1422581869,
   "bundle": "com.coolapk.app",
   "name": "酷安-分享美好科技生活",
-  "version": "16.2.3",
-  "version_id": 884976767,
-  "version_release_timestamp": "2026-04-28T05:57:03Z",
+  "version": "16.2.4",
+  "version_id": 885463305,
+  "version_release_timestamp": "2026-05-14T08:52:17Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/7b/4a/d9/7b4ad993-023f-9252-cf90-759e5b92eb99/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b2/df/58/b2df58ab-06d7-456a-7daf-62d9b84ffdc4/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/932747118_us.js
+++ Library/Caches/com.nan.cac/932747118_us.js
@@ -3,9 +3,9 @@
   "id": 932747118,
   "bundle": "com.liguangming.Shadowrocket",
   "name": "Shadowrocket",
-  "version": "2.2.80",
-  "version_id": 882526963,
-  "version_release_timestamp": "2026-02-19T01:45:26Z",
+  "version": "2.2.81",
+  "version_id": 885648402,
+  "version_release_timestamp": "2026-05-14T07:51:35Z",
   "price": "USD 2.99",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/0b/02/ca/0b02cadf-d5b7-aca7-c4f3-86e3f3bcc9d2/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d7/85/6a/d7856a18-dde8-0b60-e1e7-5019b5cb3100/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
