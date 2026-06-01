5591088621c0170668f62862aee8f2ac2b8ed811
GitHub Action <action@github.com>
2026-06-01 10:44:44 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,9 +3,9 @@
   "id": 1422581869,
   "bundle": "com.coolapk.app",
   "name": "酷安-分享美好科技生活",
-  "version": "16.2.4",
-  "version_id": 885463305,
-  "version_release_timestamp": "2026-05-14T08:52:17Z",
+  "version": "16.2.5",
+  "version_id": 886292392,
+  "version_release_timestamp": "2026-06-01T07:07:47Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b2/df/58/b2df58ab-06d7-456a-7daf-62d9b84ffdc4/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/55/97/a0/5597a01e-81e7-66ab-fdff-5d23577c52ce/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,9 +3,9 @@
   "id": 741292507,
   "bundle": "com.xingin.discover",
   "name": "rednote",
-  "version": 9.33,
-  "version_id": 886189812,
-  "version_release_timestamp": "2026-05-30T04:07:05Z",
+  "version": "9.33.2",
+  "version_id": 886340418,
+  "version_release_timestamp": "2026-06-01T08:04:10Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/01/b5/a4/01b5a420-3249-933d-641b-5cf6329a1225/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/19/28/82/19288238-556b-cc63-5ab5-d2ac00cd9bfd/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
