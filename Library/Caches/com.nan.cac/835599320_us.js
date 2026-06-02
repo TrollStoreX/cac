c99ada52f9c0bc9249bd183410c65353d6e11746
GitHub Action <action@github.com>
2026-06-02 00:30:01 +0000

    save caches

--- Library/Caches/com.nan.cac/835599320_us.js
+++ Library/Caches/com.nan.cac/835599320_us.js
@@ -3,9 +3,9 @@
   "id": 835599320,
   "bundle": "com.zhiliaoapp.musically",
   "name": "TikTok - Videos, Shop & LIVE",
-  "version": "45.3.0",
-  "version_id": 885993010,
-  "version_release_timestamp": "2026-05-26T01:18:50Z",
+  "version": "45.4.0",
+  "version_id": 886275425,
+  "version_release_timestamp": "2026-06-01T20:26:58Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/6b/c5/9f/6bc59f11-c2c3-1009-e18b-0033c4ef4d61/TikTok_AppIcon26-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/5b/02/2a/5b022a3d-7899-3d4f-7f5b-fac7fb12f109/TikTok_AppIcon26-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
