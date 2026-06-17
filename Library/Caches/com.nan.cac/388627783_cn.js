15ade1ae48c0e3cad77e22107cb69a4a6767f79a
GitHub Action <action@github.com>
2026-06-17 10:09:34 +0000

    save caches

--- Library/Caches/com.nan.cac/388627783_cn.js
+++ Library/Caches/com.nan.cac/388627783_cn.js
@@ -3,9 +3,9 @@
   "id": 388627783,
   "bundle": "com.intsig.CamScannerLite",
   "name": "扫描全能王-手机扫描仪 & PDF图片转文字",
-  "version": "7.19.0",
-  "version_id": 886567697,
-  "version_release_timestamp": "2026-06-08T03:04:17Z",
+  "version": "7.19.5",
+  "version_id": 886835520,
+  "version_release_timestamp": "2026-06-15T06:04:40Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/4a/dd/51/4add5139-f409-8b6c-8294-b127a70d1e2d/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/6d/2b/87/6d2b87c3-dbe7-fb00-d285-79c1611be88e/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/587366035_us.js
+++ Library/Caches/com.nan.cac/587366035_us.js
@@ -3,9 +3,9 @@
   "id": 587366035,
   "bundle": "com.picsart.studio",
   "name": "Picsart AI Photo Editor, Video",
-  "version": "30.1.1",
-  "version_id": 886526793,
-  "version_release_timestamp": "2026-06-15T05:23:27Z",
+  "version": "30.1.3",
+  "version_id": 887020607,
+  "version_release_timestamp": "2026-06-17T04:15:11Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/e8/bc/9e/e8bc9eb4-f291-12e7-6005-d429695506ab/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/3f/78/a9/3f78a998-dbf1-a5d5-1bc7-6071a2183983/AppIcon-0-0-1x_U007emarketing-0-8-0-sRGB-85-220.png/100x100bb.jpg"
 }
