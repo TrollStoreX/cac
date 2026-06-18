4f0bff51af7e9e5cbfa9a3b50dbeb85788eea9e0
GitHub Action <action@github.com>
2026-06-18 05:26:45 +0000

    save caches

--- Library/Caches/com.nan.cac/284882215_us.js
+++ Library/Caches/com.nan.cac/284882215_us.js
@@ -3,9 +3,9 @@
   "id": 284882215,
   "bundle": "com.facebook.Facebook",
   "name": "Facebook",
-  "version": "566.0.0",
-  "version_id": 886718549,
-  "version_release_timestamp": "2026-06-16T06:08:12Z",
+  "version": 566.1,
+  "version_id": 887024766,
+  "version_release_timestamp": "2026-06-18T03:03:09Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/47/63/2f/47632f92-daa9-69aa-e280-268788ff451d/Icon-Production-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/18/01/32/18013282-d004-b680-4fdf-e8743f570bce/Icon-Production-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,9 +3,9 @@
   "id": 741292507,
   "bundle": "com.xingin.discover",
   "name": "rednote",
-  "version": "9.34.3",
-  "version_id": 886947117,
-  "version_release_timestamp": "2026-06-15T03:02:51Z",
+  "version": "9.34.4",
+  "version_id": 887096827,
+  "version_release_timestamp": "2026-06-18T02:09:37Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/f9/02/7c/f9027c9e-e550-f729-7c80-a046af313b53/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/2c/fc/53/2cfc537d-4055-40db-f4aa-46b3c385f6d0/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
