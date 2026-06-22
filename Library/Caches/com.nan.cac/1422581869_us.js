c09e41bc7937b75abd3a6dd43a06320324e92f79
GitHub Action <action@github.com>
2026-06-22 02:11:33 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,9 +3,9 @@
   "id": 1422581869,
   "bundle": "com.coolapk.app",
   "name": "酷安-分享美好科技生活",
-  "version": "16.2.5",
-  "version_id": 886292392,
-  "version_release_timestamp": "2026-06-01T07:07:47Z",
+  "version": 16.3,
+  "version_id": 887080657,
+  "version_release_timestamp": "2026-06-22T01:14:14Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/55/97/a0/5597a01e-81e7-66ab-fdff-5d23577c52ce/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/54/86/32/548632c2-3d3f-7dc8-be4e-fcb992f1f53d/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -3,9 +3,9 @@
   "id": 544007664,
   "bundle": "com.google.ios.youtube",
   "name": "YouTube",
-  "version": "21.24.3",
-  "version_id": 886451555,
-  "version_release_timestamp": "2026-06-14T16:20:53Z",
+  "version": "21.25.5",
+  "version_id": 887168636,
+  "version_release_timestamp": "2026-06-21T23:09:01Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/20/ea/87/20ea8738-6fac-caea-ccc1-72d8375a2310/logo_youtube_2024_q4_color-0-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/4f/cb/7e/4fcb7e91-05c8-ec7f-31da-8594e37a755e/logo_youtube_2024_q4_color-0-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220.png/100x100bb.jpg"
 }
