fdaa22b72cc277ac6f6637f8f4073d68ea9564d1
GitHub Action <action@github.com>
2026-06-23 17:33:12 +0000

    save caches

--- Library/Caches/com.nan.cac/6448311069_us.js
+++ Library/Caches/com.nan.cac/6448311069_us.js
@@ -3,9 +3,9 @@
   "id": 6448311069,
   "bundle": "com.openai.chat",
   "name": "ChatGPT",
-  "version": "1.2026.160",
-  "version_id": 886784901,
-  "version_release_timestamp": "2026-06-16T14:05:27Z",
+  "version": "1.2026.167",
+  "version_id": 887096912,
+  "version_release_timestamp": "2026-06-23T15:59:51Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/e6/c7/eb/e6c7eb69-ade8-00b8-3454-d359249c6a0a/AppIcon-0-0-1x_U007epad-0-0-0-1-0-P3-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/22/59/ed/2259edf9-6bd5-a17a-c035-074aac0954d2/AppIcon-0-0-1x_U007epad-0-0-0-1-0-P3-85-220.png/100x100bb.jpg"
 }
