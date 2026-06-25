00276d75aee5ac210b737235460a18b39bfdf2d3
GitHub Action <action@github.com>
2026-06-25 04:27:24 +0000

    save caches

--- Library/Caches/com.nan.cac/350962117_cn.js
+++ Library/Caches/com.nan.cac/350962117_cn.js
@@ -3,9 +3,9 @@
   "id": 350962117,
   "bundle": "com.sina.weibo",
   "name": "微博",
-  "version": "16.6.2",
-  "version_id": 887028638,
-  "version_release_timestamp": "2026-06-17T01:56:21Z",
+  "version": "16.6.3",
+  "version_id": 887376411,
+  "version_release_timestamp": "2026-06-25T02:05:06Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/40/1d/1f/401d1f95-7a61-6e60-d3af-ef3831cd3259/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/3e/4e/48/3e4e488b-b000-b658-0c38-a5abf7ee8b81/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,9 +3,9 @@
   "id": 741292507,
   "bundle": "com.xingin.discover",
   "name": "rednote",
-  "version": 9.35,
-  "version_id": 887128785,
-  "version_release_timestamp": "2026-06-22T03:34:05Z",
+  "version": "9.35.1",
+  "version_id": 887344471,
+  "version_release_timestamp": "2026-06-25T02:59:01Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/bd/b3/5a/bdb35ac5-01e0-a4c8-56e8-d942f7235aa7/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/e4/6e/a7/e46ea769-a88e-1b3d-3c35-e49aaf872ef7/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
