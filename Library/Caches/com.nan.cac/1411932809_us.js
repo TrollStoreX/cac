115cb904e794fad90676afa6be49293f9c223216
GitHub Action <action@github.com>
2026-07-01 02:01:22 +0000

    save caches

--- Library/Caches/com.nan.cac/1411932809_us.js
+++ Library/Caches/com.nan.cac/1411932809_us.js
@@ -3,9 +3,9 @@
   "id": 1411932809,
   "bundle": "com.feng.New.WPForumClient",
   "name": "威锋-科技新闻&果粉互动交流社区",
-  "version": 7.1,
-  "version_id": 885705585,
-  "version_release_timestamp": "2026-05-15T14:42:40Z",
+  "version": "7.1.1",
+  "version_id": 887614871,
+  "version_release_timestamp": "2026-07-01T00:18:52Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/4a/65/fa/4a65face-c771-1114-7d62-14630fbab65a/AppIcon-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/1a/9f/9e/1a9f9e1f-8162-6069-cc71-cfa25003cbd9/AppIcon-0-0-1x_U007ephone-0-1-0-85-220.png/100x100bb.jpg"
 }
