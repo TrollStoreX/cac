048ed09471ee24dff825dca0e0355251cdf6d913
GitHub Action <action@github.com>
2026-07-07 08:47:50 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,9 +3,9 @@
   "id": 1422581869,
   "bundle": "com.coolapk.app",
   "name": "酷安-分享美好科技生活",
-  "version": "16.3.1",
-  "version_id": 887266867,
-  "version_release_timestamp": "2026-06-23T00:59:38Z",
+  "version": 16.4,
+  "version_id": 887894924,
+  "version_release_timestamp": "2026-07-07T05:59:14Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/67/a8/03/67a803de-b32b-f392-111f-80ef8ea67f65/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/93/76/a3/9376a370-873a-f8b8-b79c-21d667e97264/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/736536022_us.js
+++ Library/Caches/com.nan.cac/736536022_us.js
@@ -3,9 +3,9 @@
   "id": 736536022,
   "bundle": "tv.danmaku.bilianime",
   "name": "bilibili - All Your Fav Videos",
-  "version": "9.1.2",
-  "version_id": 887787835,
-  "version_release_timestamp": "2026-07-04T04:00:58Z",
+  "version": "9.2.0",
+  "version_id": 887893941,
+  "version_release_timestamp": "2026-07-07T06:58:09Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c3/84/9c/c3849c50-d4b7-e2bd-f321-fe4f72250853/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/da/fa/95/dafa95cc-4372-0c4c-3bbb-ef916ddd371d/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
