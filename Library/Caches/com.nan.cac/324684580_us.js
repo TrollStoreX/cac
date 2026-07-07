f2ee8af7d3c30f438b7e0c75ca3d008e2346fc64
GitHub Action <action@github.com>
2026-07-07 17:31:10 +0000

    save caches

--- Library/Caches/com.nan.cac/324684580_us.js
+++ Library/Caches/com.nan.cac/324684580_us.js
@@ -3,9 +3,9 @@
   "id": 324684580,
   "bundle": "com.spotify.client",
   "name": "Spotify: Music and Podcasts",
-  "version": "9.1.60",
-  "version_id": 887335599,
-  "version_release_timestamp": "2026-06-26T07:29:24Z",
+  "version": "9.1.62",
+  "version_id": 887679168,
+  "version_release_timestamp": "2026-07-07T15:28:00Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/50/c5/33/50c533fe-3854-b5ce-92c5-b2a1cb4fafe2/AppIcon-0-0-1x_U007epad-0-1-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/40/04/4e/40044e28-ff9b-e382-fc03-3425835acf16/AppIcon-0-0-1x_U007epad-0-1-0-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/389801252_us.js
+++ Library/Caches/com.nan.cac/389801252_us.js
@@ -3,9 +3,9 @@
   "id": 389801252,
   "bundle": "com.burbn.instagram",
   "name": "Instagram",
-  "version": 437.1,
-  "version_id": 887822808,
-  "version_release_timestamp": "2026-07-05T16:28:19Z",
+  "version": "437.2.0",
+  "version_id": 887866448,
+  "version_release_timestamp": "2026-07-07T15:12:53Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/b5/36/d6/b536d602-4dee-0f16-4fc1-7f6de0d10611/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/88/41/f1/8841f1a8-adc9-a54a-84ab-68f09dfe9731/Prod-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
