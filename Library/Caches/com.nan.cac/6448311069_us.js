5f1738c5efa70e8c38dde81cfaa089c51d542a15
GitHub Action <action@github.com>
2026-07-08 04:57:24 +0000

    save caches

--- Library/Caches/com.nan.cac/6448311069_us.js
+++ Library/Caches/com.nan.cac/6448311069_us.js
@@ -3,9 +3,9 @@
   "id": 6448311069,
   "bundle": "com.openai.chat",
   "name": "ChatGPT",
-  "version": "1.2026.181",
-  "version_id": 887393433,
-  "version_release_timestamp": "2026-07-07T03:04:43Z",
+  "version": "1.2026.182",
+  "version_id": 887929172,
+  "version_release_timestamp": "2026-07-08T03:59:31Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/e2/13/f0/e213f091-4dd8-ecda-dbee-3bba0a5bfaf1/AppIcon-0-0-1x_U007epad-0-0-0-1-0-P3-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/32/35/7f/32357f1a-177a-6c7f-eb79-5a5d0a1bcd95/AppIcon-0-0-1x_U007epad-0-0-0-1-0-P3-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,9 +3,9 @@
   "id": 741292507,
   "bundle": "com.xingin.discover",
   "name": "rednote",
-  "version": 9.369999999999999,
-  "version_id": 887747854,
-  "version_release_timestamp": "2026-07-04T02:18:16Z",
+  "version": "9.37.2",
+  "version_id": 887962329,
+  "version_release_timestamp": "2026-07-08T03:23:10Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/99/77/06/99770682-eb54-f8d5-751d-d79ab33e7ed9/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/c7/a5/c0/c7a5c0e7-e49a-add9-f759-c6887902b56b/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
