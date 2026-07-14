1b4088d8b33496ca5c72f664f3ae18ae428a192b
GitHub Action <action@github.com>
2026-07-14 08:58:16 +0000

    save caches

--- Library/Caches/com.nan.cac/284882215_us.js
+++ Library/Caches/com.nan.cac/284882215_us.js
@@ -3,9 +3,9 @@
   "id": 284882215,
   "bundle": "com.facebook.Facebook",
   "name": "Facebook",
-  "version": "569.0.0",
-  "version_id": 887764494,
-  "version_release_timestamp": "2026-07-07T09:38:14Z",
+  "version": "570.0.0",
+  "version_id": 887944679,
+  "version_release_timestamp": "2026-07-14T05:48:30Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/3d/77/75/3d7775af-f82f-6eb2-3e03-49ec4c77dd2b/Icon-Production-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/f7/7d/9b/f77d9b8f-1ea8-b201-791d-e06014b8b5d1/Icon-Production-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/736536022_us.js
+++ Library/Caches/com.nan.cac/736536022_us.js
@@ -3,9 +3,9 @@
   "id": 736536022,
   "bundle": "tv.danmaku.bilianime",
   "name": "bilibili - All Your Fav Videos",
-  "version": "9.2.0",
-  "version_id": 887893941,
-  "version_release_timestamp": "2026-07-07T06:58:09Z",
+  "version": "9.3.0",
+  "version_id": 888213032,
+  "version_release_timestamp": "2026-07-14T06:35:59Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/da/fa/95/dafa95cc-4372-0c4c-3bbb-ef916ddd371d/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/a3/cf/fd/a3cffdf4-a0a2-e46e-caa2-aa1d07109ebe/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
