09d95d4a68bb882f89b7d5c5f1a31ff20a2d3497
GitHub Action <action@github.com>
2026-07-28 09:49:21 +0000

    save caches

--- Library/Caches/com.nan.cac/324684580_us.js
+++ Library/Caches/com.nan.cac/324684580_us.js
@@ -3,9 +3,9 @@
   "id": 324684580,
   "bundle": "com.spotify.client",
   "name": "Spotify: Music and Podcasts",
-  "version": "9.1.66",
-  "version_id": 888015884,
-  "version_release_timestamp": "2026-07-20T07:15:00Z",
+  "version": "9.1.68",
+  "version_id": 888571550,
+  "version_release_timestamp": "2026-07-28T07:54:47Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/60/81/77/608177fe-91d3-e149-3e09-3102fc81798d/AppIcon-0-0-1x_U007epad-0-1-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/01/b4/f5/01b4f500-a216-ede2-81dd-e6b4668e53cf/AppIcon-0-0-1x_U007epad-0-1-0-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/736536022_us.js
+++ Library/Caches/com.nan.cac/736536022_us.js
@@ -3,9 +3,9 @@
   "id": 736536022,
   "bundle": "tv.danmaku.bilianime",
   "name": "bilibili - All Your Fav Videos",
-  "version": "9.4.0",
-  "version_id": 888559268,
-  "version_release_timestamp": "2026-07-21T03:53:38Z",
+  "version": "9.5.0",
+  "version_id": 888904720,
+  "version_release_timestamp": "2026-07-28T07:30:28Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c5/40/7d/c5407dd6-c2b8-978a-5e63-2bf83869186a/AppIcon-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/2b/aa/1d/2baa1daa-5b1b-26c0-5eed-2df46ae4483e/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
