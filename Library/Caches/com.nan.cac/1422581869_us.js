d5e993ff26b9aef78435b9f7a8212055cce945ae
GitHub Action <action@github.com>
2026-07-29 03:40:39 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,9 +3,9 @@
   "id": 1422581869,
   "bundle": "com.coolapk.app",
   "name": "酷安-分享美好科技生活",
-  "version": "16.5.1",
-  "version_id": 888671860,
-  "version_release_timestamp": "2026-07-23T02:34:52Z",
+  "version": "16.5.2",
+  "version_id": 888953870,
+  "version_release_timestamp": "2026-07-29T01:42:32Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/a9/97/a1/a997a1bb-b572-52c7-20ce-3a7ec54aa481/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/86/19/67/86196751-870a-c4f6-9a2e-6965a398d2a2/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/350962117_cn.js
+++ Library/Caches/com.nan.cac/350962117_cn.js
@@ -3,9 +3,9 @@
   "id": 350962117,
   "bundle": "com.sina.weibo",
   "name": "微博",
-  "version": "16.7.3",
-  "version_id": 888618362,
-  "version_release_timestamp": "2026-07-22T01:56:39Z",
+  "version": "16.7.4",
+  "version_id": 888951445,
+  "version_release_timestamp": "2026-07-29T01:59:03Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/58/8c/9d/588c9d63-0920-4edf-9d41-d2b1c3eee3eb/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/1b/f5/b0/1bf5b086-be9b-1ed0-69b1-baeef7f349f3/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
