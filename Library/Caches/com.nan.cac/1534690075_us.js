f20828e430a77dd235f3c44ed4eb6485a2768e1f
GitHub Action <action@github.com>
2026-08-07 03:41:44 +0000

    save caches

--- Library/Caches/com.nan.cac/1534690075_us.js
+++ Library/Caches/com.nan.cac/1534690075_us.js
@@ -3,9 +3,9 @@
   "id": 1534690075,
   "bundle": "tech.baye.placeholder",
   "name": "DAMA - Auto Redact Privacy",
-  "version": "26.8.0",
-  "version_id": 889049091,
-  "version_release_timestamp": "2026-08-01T05:22:20Z",
+  "version": "26.8.1",
+  "version_id": 889159623,
+  "version_release_timestamp": "2026-08-07T02:28:19Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/02/fa/97/02fa9734-dcbd-2ee9-f99c-af01e038d8a4/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/28/db/1e/28db1e77-a9d8-99ee-441c-0cc8c7aa8773/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
---
+++ Library/Caches/com.nan.cac/6446901002_us.js
@@ -0,0 +1,11 @@
+{
+  "status": 0,
+  "id": 6446901002,
+  "bundle": "com.burbn.barcelona",
+  "name": "Threads",
+  "version": 441,
+  "version_id": 888909930,
+  "version_release_timestamp": "2026-08-03T14:01:49Z",
+  "price": null,
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/e7/4c/fb/e74cfb45-827b-e1b0-1456-7298c0d01232/Prod-0-0-1x_U007ephone-0-0-0-1-0-0-P3-85-220.png/100x100bb.jpg"
+}
