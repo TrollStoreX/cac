76b84fb8356bcd842ff029390412da7cd1ed6c78
GitHub Action <action@github.com>
2026-08-08 05:28:59 +0000

    save caches

--- Library/Caches/com.nan.cac/736536022_us.js
+++ Library/Caches/com.nan.cac/736536022_us.js
@@ -3,9 +3,9 @@
   "id": 736536022,
   "bundle": "tv.danmaku.bilianime",
   "name": "bilibili - All Your Fav Videos",
-  "version": "9.6.0",
-  "version_id": 889231840,
-  "version_release_timestamp": "2026-08-07T09:26:14Z",
+  "version": "9.6.1",
+  "version_id": 889437076,
+  "version_release_timestamp": "2026-08-08T03:41:55Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/97/c5/bc/97c5bc95-c067-ace1-3753-e2cec5fb9245/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/44/ef/40/44ef4051-f1ae-3b11-9e83-edb96696940a/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
