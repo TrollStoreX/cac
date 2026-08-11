ee2faffed13a6ff212cb9c4ae0e012b67627788f
GitHub Action <action@github.com>
2026-08-11 10:41:02 +0000

    save caches

--- Library/Caches/com.nan.cac/388627783_cn.js
+++ Library/Caches/com.nan.cac/388627783_cn.js
@@ -3,9 +3,9 @@
   "id": 388627783,
   "bundle": "com.intsig.CamScannerLite",
   "name": "扫描全能王-手机扫描仪 & PDF图片转文字",
-  "version": "7.23.0",
-  "version_id": 889110415,
-  "version_release_timestamp": "2026-08-04T06:43:30Z",
+  "version": "7.23.5",
+  "version_id": 889397431,
+  "version_release_timestamp": "2026-08-11T09:18:08Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/0b/31/c5/0b31c5d4-45b5-d870-a82c-4ce1632be739/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/3c/f5/15/3cf515f6-c289-255b-a746-b93376436953/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
 }
