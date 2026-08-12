c5cd3eb98d2fe0f769e65b5f0fc4f98e21efb873
GitHub Action <action@github.com>
2026-08-12 02:50:37 +0000

    save caches

--- Library/Caches/com.nan.cac/1422581869_us.js
+++ Library/Caches/com.nan.cac/1422581869_us.js
@@ -3,9 +3,9 @@
   "id": 1422581869,
   "bundle": "com.coolapk.app",
   "name": "酷安-分享美好科技生活",
-  "version": "16.5.3",
-  "version_id": 889129987,
-  "version_release_timestamp": "2026-08-03T01:24:37Z",
+  "version": "16.5.4",
+  "version_id": 889544735,
+  "version_release_timestamp": "2026-08-12T01:27:26Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/ae/5f/54/ae5f54af-2e7c-a407-4d23-2676b3c76caf/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/9d/37/9a/9d379aaa-cf7e-dcd6-7c56-102bb4e9f5fb/AppIcon-0-0-1x_U007emarketing-0-11-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/1534690075_us.js
+++ Library/Caches/com.nan.cac/1534690075_us.js
@@ -3,9 +3,9 @@
   "id": 1534690075,
   "bundle": "tech.baye.placeholder",
   "name": "DAMA - Auto Redact Privacy",
-  "version": "26.8.2",
-  "version_id": 889422581,
-  "version_release_timestamp": "2026-08-08T15:33:32Z",
+  "version": "26.8.3",
+  "version_id": 889501039,
+  "version_release_timestamp": "2026-08-12T02:31:30Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/d4/0c/df/d40cdf39-e986-9779-52c1-b20da320a812/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/94/70/87/94708711-c1e4-8f9b-ed3a-b4c93104d296/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
