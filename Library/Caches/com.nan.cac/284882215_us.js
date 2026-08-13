1ab8b2750d030bc629b501bc0a48c07faf297ed1
GitHub Action <action@github.com>
2026-08-13 04:02:16 +0000

    save caches

--- Library/Caches/com.nan.cac/284882215_us.js
+++ Library/Caches/com.nan.cac/284882215_us.js
@@ -3,9 +3,9 @@
   "id": 284882215,
   "bundle": "com.facebook.Facebook",
   "name": "Facebook",
-  "version": "573.0.0",
-  "version_id": 888954588,
-  "version_release_timestamp": "2026-08-05T15:17:36Z",
+  "version": "574.0.0",
+  "version_id": 889352804,
+  "version_release_timestamp": "2026-08-13T02:14:14Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/72/b7/7f72b76a-5865-3da5-8dc3-71a2b75551e9/Icon-Production-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/09/1e/e0/091ee060-69ab-37fd-dee8-c62713a89ffc/Icon-Production-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/350962117_cn.js
+++ Library/Caches/com.nan.cac/350962117_cn.js
@@ -3,9 +3,9 @@
   "id": 350962117,
   "bundle": "com.sina.weibo",
   "name": "微博",
-  "version": "16.8.0",
-  "version_id": 889295198,
-  "version_release_timestamp": "2026-08-05T08:41:03Z",
+  "version": "16.8.1",
+  "version_id": 889595206,
+  "version_release_timestamp": "2026-08-13T01:54:02Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/89/ce/e2/89cee22d-0b95-32a7-36b4-859253ece4ff/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/33/88/9d/33889d81-e70a-ecb7-b1ae-be840f32f987/WeiboAppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
