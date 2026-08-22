c5b9cf18f21b8901368b3e02833973a3455b70a6
GitHub Action <action@github.com>
2026-08-22 00:02:59 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -3,9 +3,9 @@
   "id": 333903271,
   "bundle": "com.atebits.Tweetie2",
   "name": "X",
-  "version": 12.19,
-  "version_id": 889952680,
-  "version_release_timestamp": "2026-08-20T21:24:42Z",
+  "version": "12.19.1",
+  "version_id": 890077605,
+  "version_release_timestamp": "2026-08-21T22:37:29Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/8f/13/30/8f1330ae-8e75-8511-c045-357248f0a433/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/e0/2d/39/e02d39e9-0bbb-c666-a51e-dc96634a3135/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/544007664_us.js
+++ Library/Caches/com.nan.cac/544007664_us.js
@@ -3,9 +3,9 @@
   "id": 544007664,
   "bundle": "com.google.ios.youtube",
   "name": "YouTube",
-  "version": "21.33.5",
-  "version_id": 889742276,
-  "version_release_timestamp": "2026-08-19T16:27:10Z",
+  "version": "21.33.6",
+  "version_id": 890011794,
+  "version_release_timestamp": "2026-08-21T23:05:37Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/bc/f3/27/bcf32768-0d66-6d54-f426-040b4688b6e7/logo_youtube_2024_q4_color-0-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/98/7c/9a/987c9a66-ffa1-de65-0e55-e6eaa817c19a/logo_youtube_2024_q4_color-0-0-1x_U007emarketing-0-0-0-7-0-0-0-85-220.png/100x100bb.jpg"
 }
