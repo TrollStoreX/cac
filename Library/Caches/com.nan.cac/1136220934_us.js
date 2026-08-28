dd0345e34f98db88a583248881dd1bfd497dca94
GitHub Action <action@github.com>
2026-08-28 05:27:59 +0000

    save caches

--- Library/Caches/com.nan.cac/1136220934_us.js
+++ Library/Caches/com.nan.cac/1136220934_us.js
@@ -3,9 +3,9 @@
   "id": 1136220934,
   "bundle": "com.firecore.infuse",
   "name": "Infuse",
-  "version": "8.5.2",
-  "version_id": 890100432,
-  "version_release_timestamp": "2026-08-26T19:13:23Z",
+  "version": "8.5.3",
+  "version_id": 890390354,
+  "version_release_timestamp": "2026-08-28T01:04:34Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c8/88/eb/c888ebae-b465-6b80-4fb0-720b1a47fece/AppIcon-0-0-1x_U007epad-0-11-0-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/35/31/5b/35315b8d-75cc-1be3-ffc8-e6c579c45d45/AppIcon-0-0-1x_U007epad-0-11-0-0-0-0-85-220.png/100x100bb.jpg"
 }
