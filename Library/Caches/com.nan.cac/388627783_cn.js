e1fb1f18c2e2bc88436953ee1a2a5f5a8b326b53
GitHub Action <action@github.com>
2026-08-31 05:35:22 +0000

    save caches

--- Library/Caches/com.nan.cac/388627783_cn.js
+++ Library/Caches/com.nan.cac/388627783_cn.js
@@ -3,9 +3,9 @@
   "id": 388627783,
   "bundle": "com.intsig.CamScannerLite",
   "name": "扫描全能王-手机扫描仪 & PDF图片转文字",
-  "version": "7.24.0",
-  "version_id": 889753789,
-  "version_release_timestamp": "2026-08-18T02:55:14Z",
+  "version": "7.25.0",
+  "version_id": 890053917,
+  "version_release_timestamp": "2026-08-31T02:19:21Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/f7/ae/9f/f7ae9f20-d5ed-fdb8-c75e-7b5844536878/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/8d/07/ea/8d07eabd-76eb-ba47-a010-0adc5cd99849/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,9 +3,9 @@
   "id": 741292507,
   "bundle": "com.xingin.discover",
   "name": "rednote",
-  "version": "9.44.1",
-  "version_id": 890116152,
-  "version_release_timestamp": "2026-08-23T02:52:10Z",
+  "version": 9.449999999999999,
+  "version_id": 890237252,
+  "version_release_timestamp": "2026-08-31T01:53:15Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/51/bd/bf/51bdbf47-bf6c-e078-afb1-d2737a89efde/AppIcon-0-0-1x_U007epad-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7d/a3/ab/7da3abfc-673c-9677-bbf8-a04a30b5ae42/AppIcon-0-0-1x_U007epad-0-11-0-85-220.png/100x100bb.jpg"
 }
