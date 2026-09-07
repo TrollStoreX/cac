7f8e5a983dca9c341cd164ca411c0a830f6c82b1
GitHub Action <action@github.com>
2026-09-07 04:36:28 +0000

    save caches

--- Library/Caches/com.nan.cac/333903271_us.js
+++ Library/Caches/com.nan.cac/333903271_us.js
@@ -3,9 +3,9 @@
   "id": 333903271,
   "bundle": "com.atebits.Tweetie2",
   "name": "X",
-  "version": 12.24,
-  "version_id": 890829288,
-  "version_release_timestamp": "2026-09-05T18:31:30Z",
+  "version": "12.24.1",
+  "version_id": 890885118,
+  "version_release_timestamp": "2026-09-06T23:55:43Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/83/6f/af/836faf36-eb77-9ac5-4d0d-c38b77970b8a/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/02/f0/d0/02f0d032-7612-8794-935a-947f2771bb04/ProductionAppIcon-0-0-1x_U007emarketing-0-8-0-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/388627783_cn.js
+++ Library/Caches/com.nan.cac/388627783_cn.js
@@ -3,9 +3,9 @@
   "id": 388627783,
   "bundle": "com.intsig.CamScannerLite",
   "name": "扫描全能王-手机扫描仪 & PDF图片转文字",
-  "version": "7.25.0",
-  "version_id": 890053917,
-  "version_release_timestamp": "2026-08-31T02:19:21Z",
+  "version": "7.25.5",
+  "version_id": 890823925,
+  "version_release_timestamp": "2026-09-07T02:25:12Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/8d/07/ea/8d07eabd-76eb-ba47-a010-0adc5cd99849/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/26/ff/f2/26fff2c0-729b-5216-0c13-7d0499f10940/AppIconLite-0-0-1x_U007ephone-0-6-0-0-sRGB-85-220.png/100x100bb.jpg"
 }
