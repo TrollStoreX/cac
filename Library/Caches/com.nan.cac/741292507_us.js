4ba019dbf205c51cd3a046782b2a1b5572aa5fec
GitHub Action <action@github.com>
2026-09-07 05:18:38 +0000

    save caches

--- Library/Caches/com.nan.cac/741292507_us.js
+++ Library/Caches/com.nan.cac/741292507_us.js
@@ -3,9 +3,9 @@
   "id": 741292507,
   "bundle": "com.xingin.discover",
   "name": "rednote",
-  "version": "9.45.2",
-  "version_id": 890652084,
-  "version_release_timestamp": "2026-09-03T02:20:04Z",
+  "version": 9.460000000000001,
+  "version_id": 890776184,
+  "version_release_timestamp": "2026-09-07T03:21:11Z",
   "price": null,
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/07/21/c2/0721c20d-368e-e63a-3685-592cf86c11e3/AppIcon-0-0-1x_U007epad-0-11-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/35/d7/a7/35d7a7ee-1772-9f5f-cebf-c6430ef36235/AppIcon-0-0-1x_U007epad-0-11-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/932747118_us.js
+++ Library/Caches/com.nan.cac/932747118_us.js
@@ -3,9 +3,9 @@
   "id": 932747118,
   "bundle": "com.liguangming.Shadowrocket",
   "name": "Shadowrocket",
-  "version": "2.2.90",
-  "version_id": 886813649,
-  "version_release_timestamp": "2026-07-07T21:12:37Z",
+  "version": "2.2.92",
+  "version_id": 890167352,
+  "version_release_timestamp": "2026-09-07T04:14:10Z",
   "price": "USD 2.99",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/44/d3/df/44d3df14-cf72-37d1-6e61-582f7023b90e/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/64/24/2d/64242de2-a763-914f-c686-8777445b83a3/AppIcon-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/100x100bb.jpg"
 }
